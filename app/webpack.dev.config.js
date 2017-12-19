47855b7c67145f20af37dc0f5229fc7ca39be25d
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2017-12-19 15:58:33 -0600

    webpack dev config

---
+++ app/webpack.dev.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client', 
+    path.resolve(__dirname, 'src', 'index.jsx')
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist/js'),
+    filename: 'app.js',
+    publicPath: '/dist/'
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            'env',
+            'react',
+            'react-hmre'
+          ],
+          plugins: [
+            'transform-object-rest-spread'
+          ]
+        }
+      }],
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+}
