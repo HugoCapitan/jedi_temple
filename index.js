e5c68b803043d159ca0ca4e2ad2906ccd46be1e4
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2017-12-19 16:02:36 -0600

    using webpack hot middleware in server

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const environment = process.env.NODE_ENV
 const port = (environment === 'production') ? 80 : 3000
 const server = express()
 
+if (environment === 'development') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  
+  const webpackConfig = require(path.resolve(__dirname, 'app', 'webpack.dev.config'))
+  const compiler = webpack(webpackConfig)
+
+  server.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }))
+
+  server.use(webpackHotMiddleware(compiler))
+}
+
 server.use('/dist/', express.static(path.resolve(__dirname, 'dist')))
 
 server.get('/', (req, res) => {
