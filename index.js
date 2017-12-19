a3177634775f466f0b2c608d38618ab295cdb42c
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2017-12-19 14:57:23 -0600

    configuring / route and /dist/ statics on server

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const environment = process.env.NODE_ENV
 const port = (environment === 'production') ? 80 : 3000
 const server = express()
 
+server.use('/dist/', express.static(path.resolve(__dirname, 'dist')))
+
 server.get('/', (req, res) => {
-  res.send('Hi')
+  res.sendFile(path.resolve(__dirname, 'app/public', 'index.html'))
 })
 
 server.listen(port, () => {
