99155cf6e81add6dcc7d460a918eb493f7d0da55
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-06 11:21:33 -0600

    adding reservation model

---
+++ app/api/models/Reservation.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ReservationSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  plan: { // Short or Long
+    type: String,
+    required: true
+  },
+  arrive_date: {
+    type: Date,
+    required: true
+  },
+  departure_date: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: Number,
+    required: true
+  },
+  created_at: Date,
+  updated_at: Date
+})
+
+ReservationSchema.pre('save', (next) => {
+  next()
+})
+
+const Reservation = mongoose.model('Reservation', ReservationSchema)
+
+module.exports = Reservation
---
+++ app/api/models/tests/Reservation.js
@@ -0,0 +1,15 @@
+const Reservation = require('../Reservation')
+
+describe('Reservation model', () => {
+
+  test('Should be valid')
+
+  test('Should be invalid if any missing field')
+
+  test('Should be invalid if arriving after depaarture')
+
+  test('Should be invalid if wrong plan')
+
+  test('Should be invalid if email is not an email')
+
+})
