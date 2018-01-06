4644b106f9623e21d93a667f0f4d555a1ecd61a5
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-06 14:20:13 -0600

    first address test cases

---
+++ app/api/models/tests/Address.test.js
@@ -0,0 +1,9 @@
+const Address = require('../Address')
+
+describe('Address model', () => {
+  
+  test('Should be valid')
+
+  test('Should be invalid if missing: name, email, country, city, state, zip, address_line_1')
+
+})
