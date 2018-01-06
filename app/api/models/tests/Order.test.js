27faa89989026c10b33a088ec01550e0f37f0dec
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-06 15:56:39 -0600

    implementing order tests

--- app/api/models/tests/Order.test.js
+++ app/api/models/tests/Order.test.js
@@ -1,5 +1,6 @@
 const Order = require('../Order')
 
+const { howManyKeys } = require('../../utils')
 const { getValidOrder } = require('../../utils/models')
 
 describe('Order model', () => {
@@ -8,19 +9,53 @@ describe('Order model', () => {
   beforeEach(() => { setupTest() })
 
   test('Should be valid', () => {
-    const m = new Order( getValidOrder() )
+    const m = new Order( validOrder )
     const v = m.validateSync()
 
     expect(v).toBeFalsy()
   })
 
-  test('Should be invalid if no: email, products, status, shipping, shipping_address, billing_address')
+  test('Should be invalid if no: email, status, products, shipping, shipping_address, billing_address', () => {
+    const m = new Order({ })
+    const v = m.validateSync()
 
-  test('Should be invalid if email wrong format')
+    expect(howManyKeys(v.errors)).toBe(6)
+    expect(v.errors.email).toBeTruthy()
+    expect(v.errors.status).toBeTruthy()
+    expect(v.errors.products).toBeTruthy()
+    expect(v.errors.shipping).toBeTruthy()
+    expect(v.errors.billing_address).toBeTruthy()
+    expect(v.errors.shipping_address).toBeTruthy()
+  })
 
-  test('Should be invalid if wrong address if for billing or shipping')
+  test('Should be invalid if email wrong format', () => {
+    const m = new Order( Object.assign( validOrder, { email: 'thisaintanemail' } ) )
+    const v = m.validateSync()
 
-  test('Should be invalid if malformed product')
+    expect(howManyKeys(v.errors)).toBe(1)
+    expect(v.errors.email).toBeTruthy()
+  })
+
+  test('Should be invalid if wrong address id if for billing or shipping', () => {
+    const m = new Order( Object.assign( validOrder, { shipping_address: 'wrong id babe', billing_address: 'haha' } ) )
+    const v = m.validateSync()
+
+    expect(howManyKeys(v.errors)).toBe(2)
+    expect(v.errors.billing_address).toBeTruthy()
+    expect(v.errors.shipping_address).toBeTruthy()
+  })
+
+  test('Should be invalid if malformed product', () => {
+    const malformed = getValidOrder()
+    malformed.products[0].quantity = 'whatup'
+    
+    const m = new Order(malformed)
+    const v = m.validateSync()
+
+    console.log(v.errors)
+
+    expect(howManyKeys(v.errors)).toBe(1)
+  })
 
   function setupTest() {
     validOrder = getValidOrder()
