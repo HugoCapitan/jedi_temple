c34151b9b50df0dd7aea7cf310190d940f39a347
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-09 10:25:34 -0600

    Adding all functions to customField controller

--- app/api/controllers/customFieldCtrl.js
+++ app/api/controllers/customFieldCtrl.js
@@ -3,7 +3,13 @@ const productCtrl = require('./productCtrl')
 
 module.exports = {
   create,
-  apiAll
+  remove,
+  update,
+  apiAll,
+  apiCreate,
+  apiRead,
+  apiRemove,
+  apiUpdate
 }
 
 async function create(fieldObj) {
@@ -42,10 +48,26 @@ async function remove(id) {
 }
 
 async function update(id, newFieldObj) {
+  // Type is not modifyiable
   try {
-
+    const updatedCustomField = await CustomField.findOneAndUpdate({slug: slug}, newFieldObj)
+    // Update Products with thit custom if necessary
+    Object.assign({}, updatedCustomField, newField);
+    return updatedCustomField
   } catch (e) {
+    if (e.customOrigin) throw e
+
+    e.customOrigin = 'Field'
+    if (e.name === 'ValidationError')
+      e.customMessage = 'Validation Error'
+    else if (e.name === "CastError")
+      e.customMessage = `CustomField ${slug} not found`
+    else if (e.code === 11000)
+      e.customMessage = 'Duplicated Name'
+    else
+      e.customMessage = 'Unexpected Error'
     
+    throw e
   }
 }
 
@@ -55,5 +77,84 @@ async function apiAll(req, res) {
     res.status(200).json(customFields)
   } catch (e) {
     utils.sendError(500, 'Unexpected Error', e, res)
+  }
+}
+
+async function apiCreate (req, res) {
+  try {
+    let newCustomField = await new CustomField(req.body).save()
+    res.status(200).json(newCustomField)
+  } catch (e) {
+    if (e.name === 'ValidationError')
+      utils.sendError(403, 'Validation Error', e, res)
+    else if (e.code === 11000)
+      utils.sendError(409, 'Duplicated Name', e, res)
+    else
+      utils.sendError(500, 'Unexpected Error', e, res)
+  }
+}
+
+async function apiRead (req, res) {
+  try {
+    const foundCustomField = await CustomField.findById(req.params.id)
+    if (!foundCustomField) {
+      let notFoundError = new Error(`CustomField ${req.params.id} not found`)
+      notFoundError.name = 'NotFoundError'
+      throw notFoundError
+    }
+
+    res.status(200).json(foundCustomField);   
+  } catch(e) {
+    if (e.name === 'NotFoundError')
+      utils.sendError(404, `CustomField ${req.params.id} not found`, e, res)
+    else
+      utils.sendError(500, 'Unexpected Error', e, res)
+  }
+}
+
+async function apiRemove (req, res) {
+  try {
+    const removedCustomField = await CustomField.findByIdAndRemove(req.params.id)
+
+    if (!removedCustomField) {
+      let notFoundError = new Error(`CustomField ${req.params.id} not found`)
+      notFoundError.name = "NotFoundError"
+      throw notFoundError
+    }
+
+    await productCtrl.removeAllProductsCustom(removedCustomField.slug)
+
+    res.status(200).send(`CustomField ${removedCustomField._id} deleted`)
+  } catch (e) {
+    if (e.customOrigin === 'Product')
+      utils.sendError(500, 'Products Update Error', e, res)
+    else if (e.name === 'CastError' || e.name === 'NotFoundError')
+      utils.sendError(404, `CustomField ${req.params.id} not found`, e, res)
+    else
+      utils.sendError(500, 'Unexpected Error', e, res)
+  }
+}
+
+async function apiUpdate (req, res) {
+  try {
+    const updatedCustomField = await CustomField.findByIdAndUpdate(req.params.id, req.body)
+
+    // Update all products
+    
+    Object.assign({}, updatedCustomField, req.body);
+    res.status(200).json(updatedCustomField);
+  } catch (e) {
+
+    if (e.customOrigin === 'Product') 
+      utils.sendError(500, 'Products Update Error', e, res);
+    else if (e.name === 'ValidationError')
+      utils.sendError(403, 'Validation Error', e, res);
+    else if (e.name === 'CastError')
+      utils.sendError(404, `CustomField ${req.params.id} not found`, e, res);
+    else if (e.code === 11000)
+      utils.sendError(409, 'Duplicated Name', e, res);
+    else
+      utils.sendError(500, 'Unexpected Error', e, res);
+
   }
 }
