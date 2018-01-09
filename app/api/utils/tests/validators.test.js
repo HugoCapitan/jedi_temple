b7fb76720c68fafad35528a254f48ca900dd548a
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-08 20:58:45 -0600

    starting testing for validators

---
+++ app/api/utils/tests/validators.test.js
@@ -0,0 +1,12 @@
+const validators = require('../validators')
+
+describe('Validators utils', () => {
+
+  describe('areMinMaxValidator', () => {
+    test('Should return false if a param isnt Number')
+    test('Should return false if missing param')
+    test('Should return false if first > last')
+    test('Should return true')
+  })
+
+})
