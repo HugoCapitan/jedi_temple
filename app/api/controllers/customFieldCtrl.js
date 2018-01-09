fd57684fd20dadb873159871ea3683418e2cc6b9
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-08 23:59:23 -0600

    first api methods on customFieldCtrl

--- app/api/controllers/customFieldCtrl.js
+++ app/api/controllers/customFieldCtrl.js
@@ -1,13 +1,14 @@
 const CustomField = require('../models/CustomField')
+const productCtrl = require('./productCtrl')
 
 module.exports = {
   create,
   apiAll
 }
 
-async function create(fieldToAdd) {
+async function create(fieldObj) {
   try {
-    const newCustomField = await new CustomField(fieldToAdd).save()
+    const newCustomField = await new CustomField(fieldObj).save()
     return newCustomField
   } catch (e) {
     e.customOrigin = 'Field'
@@ -19,6 +20,32 @@ async function create(fieldToAdd) {
       e.customMessage = 'Unexpected Error'
 
     throw e;
+  }
+}
+
+async function remove(id) {
+  try {
+    const delCustomField = await CustomField.findByIdAndRemove(id)
+    await productCtrl.removeAllProductsCustom(id)
+    return delCustomField
+  } catch (e) {
+    if (e.customOrigin) throw e // <- error from product
+
+    e.customOrigin = 'Field'
+    if (e.name === 'CastError')
+      e.customMessage = `CustomField ${id} not found`
+    else
+      e.customMessage = 'Unexpected Error'
+
+    throw e
+  }
+}
+
+async function update(id, newFieldObj) {
+  try {
+
+  } catch (e) {
+    
   }
 }
 
