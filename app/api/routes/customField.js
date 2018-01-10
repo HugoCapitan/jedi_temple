10e9a7b5ed6c76f865b037935770518d35ca8e76
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-09 22:08:39 -0600

    Full routes for customFieldCtrl

--- app/api/routes/customField.js
+++ app/api/routes/customField.js
@@ -3,4 +3,9 @@ const customFieldCtrl = require('../controllers/customFieldCtrl')
 module.exports = app => {
   app.route('/custom_fields/')
     .get(customFieldCtrl.apiAll)
+    .post(customFieldCtrl.apiCreate)
+  app.route('/custom_fields/:id')
+    .get(customFieldCtrl.apiRead)
+    .put(customFieldCtrl.apiUpdate)
+    .delete(customFieldCtrl.apiRemove)
 }
