888ac45be0167e56aba66a7c9364b36075b5231e
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-09 23:58:18 -0600

    fixing picture route

--- app/api/routes/picture.js
+++ app/api/routes/picture.js
@@ -1,10 +1,10 @@
 const pictureCtrl = require('../controllers/pictureCtrl')
 
 module.exports = app => {
-  app.route('/ROUTE/')
+  app.route('/picture/')
     .get(pictureCtrl.apiAll)
     .post(pictureCtrl.apiCreate)
-  app.route('/ROUTE/:id')
+  app.route('/picture/:id')
     .get(pictureCtrl.apiRead)
     .put(pictureCtrl.apiUpdate)
     .delete(pictureCtrl.apiRemove)
