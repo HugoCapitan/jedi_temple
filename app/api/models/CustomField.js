cf715d4b4694f92de7f008a2a3aaf7120a870121
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-13 09:31:54 -0600

    using self for correct context management

--- app/api/models/CustomField.js
+++ app/api/models/CustomField.js
@@ -76,36 +76,44 @@ const CustomFieldSchema = new Schema({
 
 CustomFieldSchema._middlewareFuncs = {
   async preSave(next) {
-    await preSaveValidations.call(this, next)
+    const self = this
+    await preSaveValidations.call(self, next)
 
-    this.slug       = slugify(this.name)
+    self.slug       = slugify(self.name)
 
-    if (this._values.length > this.values.length) {
-      const removed = this._values.find(cId => 
-        !this.values.find(cVal => cVal._id == cId)
+    if (self._values.length > self.values.length) {
+      const removed = self._values.find(cId => 
+        !self.values.find(cVal => cVal._id == cId)
       )
 
-      const productsToMod = await Product.find({ customs: { $elemMatch: { custom_id: this._id, value_id: removed } } })
-      console.log(productsToMod)
+      const productsToMod = await Product.find({ 
+        customs: { $elemMatch: { custom_id: self._id, value_id: removed } } 
+      })
+
+      productsToMod.forEach(async (prod) => {
+        prod.customs.pull({ custom_id: self._id })
+        await prod.save()
+      })
     }
 
-    this._values    = this.values.map(val => val._id)    
+    self._values    = self.values.map(val => val._id)    
 
     const currentDate = new Date()
 
-    this.updated_at = currentDate
-    if (!this.created_at)
-      this.created_at = currentDate
+    self.updated_at = currentDate
+    if (!self.created_at)
+      self.created_at = currentDate
 
     next()
   },
   preUpdate(next) {
-    preUpdateValidations.call(this, next)
+    const self = this
+    preUpdateValidations.call(self, next)
 
     const currentDate = new Date()
-    this._update.updated_at = currentDate
+    self._update.updated_at = currentDate
 
-    if (this._update.name) this._update.slug = slugify(this._update.name)
+    if (self._update.name) self._update.slug = slugify(self._update.name)
 
     next()
   },
@@ -127,28 +135,29 @@ const CustomField = mongoose.model('CustomField', CustomFieldSchema)
 module.exports = CustomField
 
 async function preSaveValidations(next) {
-  if (!this.isNew && this.isModified('slug')) {
+  const self = this
+  if (!self.isNew && self.isModified('slug')) {
     let err = new Error('Slug is not updatable')
     err.name = 'ValidationError'
     next(err)
   }
-  if (!this.isNew && this.isModified('type')) {
+  if (!self.isNew && self.isModified('type')) {
     let err = new Error('Type is not updatable')
     err.name = 'ValidationError'
     next(err)
   }
-  if (!this.isNew && this.isModified('min')) {
+  if (!self.isNew && self.isModified('min')) {
     let err = new Error('Min should be modified via update, not save.')
     err.name = 'ValidationError'
     next(err)
   }
-  if (!this.isNew && this.isModified('max')) {
+  if (!self.isNew && self.isModified('max')) {
     let err = new Error('Max should be modified via update, not save.')
     err.name = 'ValidationError'
     next(err)
   }
-  if (this.values) {
-    let valCount = this.values.reduce((acc, val) => {
+  if (self.values) {
+    let valCount = self.values.reduce((acc, val) => {
       !!acc[val.value] ? ++acc[val.value] : acc[val.value] = 1
       return acc
     }, {})
@@ -163,12 +172,12 @@ async function preSaveValidations(next) {
 }
 
 function preUpdateValidations(next) {
-  if (this._update.hasOwnProperty('slug')) {
+  if (self._update.hasOwnProperty('slug')) {
     err = new Error('Slug is not updatable')
     err.name = 'ValidationError'
     next(err)
   } 
-  if (this._update.hasOwnProperty('type')) {
+  if (self._update.hasOwnProperty('type')) {
     err = new Error('Type is not updatable')
     err.name = 'ValidationError'
     next(err)
