d87e78f1215751246d04a271763a3767b633ed26
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-17 23:26:30 -0600

    testing and implementing preSave middleware in HMPRoduct

--- app/api/models/HMProduct.js
+++ app/api/models/HMProduct.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const common = require('../utils')
+const uCommon = require('../utils')
 
 const HandmadeMaterialSchema = require('./schemas/HandmadeMaterialSchema')
 
@@ -20,6 +20,45 @@ const HMProductSchema = new Schema({
   updated_at: Date
 })
 
+HMProductSchema._middlewareFuncs = {
+  preSave(next) {
+    const self = this
+    const currentDate = new Date()
+
+    self.updated_at = currentDate
+    if (!self.created_at) self.created_at = currentDate
+
+    self.slug = uCommon.slugify(self.name)
+
+    if (self.materials) {
+      const materialsCount = self.materials.reduce((matacc, material) => {
+
+        const modelsCount = material.models.reduce((modacc, model) => {
+          !!modacc[model] ? ++modacc[model] : modacc[model] = 1
+          return modacc
+        }, {})
+
+        if (Object.values( modelsCount ).find( v => v > 1 )) {
+          const err = new Error(`Duplicated value for models in material ${material.name} in ${self.name} HMProduct`)
+          err.name = 'ValidationError'
+          next(err)
+        }
+
+        !!matacc[material.material] ? ++matacc[material.material] : matacc[material.material] = 1
+        return matacc
+      }, {})
+  
+      if (Object.values( materialsCount ).find( v => v > 1 )) {
+        let err = new Error(`Duplicated value for materials in ${self.name} HMProduct`)
+        err.name = 'ValidationError'
+        next(err)
+      }
+    }
+
+    next()
+  }
+}
+
 HMProductSchema.pre('save', (next) => {
   const currentDate = new Date
 
@@ -27,7 +66,7 @@ HMProductSchema.pre('save', (next) => {
   if (!this.created_at) 
     this.created_at = currentDate
 
-  this.slug = common.slugify(this.name)
+  this.slug = uCommon.slugify(this.name)
 
   next()
 })
--- app/api/models/tests/HMProduct.test.js
+++ app/api/models/tests/HMProduct.test.js
@@ -1,13 +1,15 @@
+const moment = require('moment')
 const HMProduct = require('../HMProduct')
 const Product = require('../Product')
 
-const { howManyKeys } = require('../../utils')
-const { getValidHMProduct, getValidProduct } = require('../../utils/validSchemas')
+const uCommon = require('../../utils')
+const valid = require('../../utils/validators')
+const uSchemas = require('../../utils/validSchemas')
 
 describe('HMProduct Model', () => {
   let validHMProduct
 
-  beforeEach(() => { validHMProduct = getValidHMProduct() })
+  beforeEach(() => { validHMProduct = uSchemas.getValidHMProduct() })
 
   test('Should be valid', () => {
     const m = new HMProduct(validHMProduct)
@@ -20,21 +22,21 @@ describe('HMProduct Model', () => {
     const m = new HMProduct( Object.assign(validHMProduct, { name: undefined }) )
     const v = m.validateSync()
 
-    expect(howManyKeys(v.errors)).toBe(1)
+    expect(uCommon.howManyKeys(v.errors)).toBe(1)
     expect(v.errors.name).toBeTruthy()
   })
 
   describe('Material subschema', () => {
 
     test('Should be invalid if material missing price or material', () => {
-      const malformedMaterial = getValidHMProduct()
+      const malformedMaterial = uSchemas.getValidHMProduct()
       malformedMaterial.materials[0].price = undefined
       malformedMaterial.materials[0].material = undefined
 
       const m = new HMProduct( malformedMaterial )
       const v = m.validateSync()
 
-      expect(howManyKeys(v.errors)).toBe(2)
+      expect(uCommon.howManyKeys(v.errors)).toBe(2)
       expect(v.errors['materials.0.material']).toBeTruthy()
       expect(v.errors['materials.0.price']).toBeTruthy()
     })
@@ -42,18 +44,145 @@ describe('HMProduct Model', () => {
   })
 
   describe('preSave Middleware', () => {
+    let next
+    const bindMiddleware = context => 
+      HMProduct.schema._middlewareFuncs.preSave.bind(context)
 
-    test('Should call next')
+    beforeEach(() => { 
+      next = jest.fn(err => { if (err) throw err })
+    })
 
-    test('Materials should iterate materials for duplicates and call next with error')
+    test('Should call next', () => {
+      const context = { name: 'bracelet' }
 
-    test('Should iterate on every material\'s models for duplicates and call next with error')
+      const boundMiddlewareFunc = bindMiddleware(context)
+
+      boundMiddlewareFunc(next)
+
+      expect( next.mock.calls.length ).toBe(1)
+    })
+
+    test('Should add created_at and updated_at', async () => {
+      const context = { name: 'bracelet' }
+
+      const boundMiddlewareFunc = bindMiddleware(context)
+
+      boundMiddlewareFunc(next)
+
+      expect( valid.isThisMinute(context.created_at) ).toBeTruthy()
+      expect( valid.isThisMinute(context.updated_at) ).toBeTruthy()
+    })
+
+    test('Should modify updated_at but not created_at', async () => {
+      const creationDate = moment().subtract(1, 'weeks').toDate()
+      const context = { name: 'bracelet', created_at: creationDate, updated_at: creationDate }
+
+      const boundMiddlewareFunc = bindMiddleware(context)
+
+      boundMiddlewareFunc(next)
+      
+      expect( context.created_at ).toBe(creationDate)
+      expect( valid.isThisMinute(context.updated_at) ).toBeTruthy()
+    })
+
+    test('Materials should iterate materials for duplicates and call next with error', () => {
+      const context = { 
+        name: 'canastita', 
+        materials: [{ 
+          name: 'olakease',
+          models: []
+        }, { 
+          name: 'hola que haces',
+          models: [] 
+        }, { 
+          name: 'olakease',
+          models: []
+        }] 
+      }
+
+      const boundMiddlewareFunc = bindMiddleware(context)
+
+      try {
+        boundMiddlewareFunc(next)
+      } catch (e) {
+        expect( next.mock.calls.length ).toBe(1)
+        expect( next.mock.calls[0][0].name ).toBe('ValidationError')
+        expect( next.mock.calls[0][0].message ).toBe('Duplicated value for materials in canastita HMProduct')
+      }
+    })
+
+    test('Should iterate on every material\'s models for duplicates and call next with error', () => {
+      const context = { 
+        name: 'canastita', 
+        materials: [{ 
+          name: 'olakease',
+          models: ['nadanada', 'mucho', 'nadanada']
+        }, { 
+          name: 'hola que haces',
+          models: [] 
+        }] 
+      }
+
+      const boundMiddlewareFunc = bindMiddleware(context)
+
+      try {
+        boundMiddlewareFunc(next)
+      } catch (e) {
+        expect( next.mock.calls.length ).toBe(1)
+        expect( next.mock.calls[0][0].name ).toBe('ValidationError')
+        expect( next.mock.calls[0][0].message ).toBe('Duplicated value for models in material olakease in canastita HMProduct')
+      }
+    })
 
   })
 
   describe('preUpdate Middleware', () => {
+    let next
+    const bindMiddleware = context => 
+      HMProduct.schema._middlewareFuncs.preUpdate.bind(context)
 
-    test('Should call next')
+    beforeEach(() => { 
+      next = jest.fn(err => { if (err) throw err })
+    })
+
+    test('Should call next', () => {
+      const context = { name: 'bracelet' }
+
+      const boundMiddlewareFunc = bindMiddleware({ _udpate: context })
+
+      boundMiddlewareFunc(next)
+
+      expect( next.mock.calls.length ).toBe(1)
+    })
+
+    // test('Should add created_at and updated_at', async () => {
+    //   const context = validClient
+
+    //   const boundMiddlewareFunc = Client.schema._middlewareFuncs.preSave.bind(context)
+    //   const next = jest.fn()
+
+    //   await boundMiddlewareFunc(next)
+
+    //   expect( isThisMinute(validClient.created_at) ).toBeTruthy()
+    //   expect( isThisMinute(validClient.updated_at) ).toBeTruthy()
+    // })
+
+    // test('Should modify updated_at but not created_at', async () => {
+    //   const creationDate = moment().subtract(1, 'weeks').toDate()
+    //   Object.assign( validClient, { created_at: creationDate, updated_at: creationDate } )
+
+    //   const context = validClient
+
+    //   const boundMiddlewareFunc = Client.schema._middlewareFuncs.preSave.bind(context)
+    //   const next = jest.fn()
+
+    //   await boundMiddlewareFunc(next)
+      
+    //   expect( validClient.created_at ).toBe(creationDate)
+    //   expect( isThisMinute(validClient.updated_at) ).toBeTruthy()
+    // })
+
+    test('Should update updated_at date')
 
     test('Materials should iterate materials for duplicates and call next with error')
 
