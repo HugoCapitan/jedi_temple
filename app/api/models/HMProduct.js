015cdb3d6b605df09fd1f8d953101bdc6c9570ca
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-18 12:26:24 -0600

    implementing preRemove and preUpdate middleware

--- app/api/models/HMProduct.js
+++ app/api/models/HMProduct.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const Store = require('./Store')
+
 const uCommon = require('../utils')
 
 const HandmadeMaterialSchema = require('./schemas/HandmadeMaterialSchema')
@@ -56,20 +58,49 @@ HMProductSchema._middlewareFuncs = {
     }
 
     next()
+  },
+  preUpdate(next) {
+    const self = this
+
+    self._update.updated_at = new Date()
+
+    if (self._update.materials) {
+      const err = new Error('Materials should be updated via HMProduct.save')
+      err.name = 'ValidationError'
+      next(err)
+    }
+
+    next()
+  },
+  async preRemove(next) {
+    const self = this
+    try {
+      const storesToModify = await Store.find({ hm_products: self._conditions._id })
+      let saves = []
+
+      for (const store of storesToModify) {
+        store.hm_products.pull(self._conditions._id)
+        saves.push(store.save())
+      }
+
+      Promise.all(saves)
+      .then(results => {
+        next()
+      })
+      .catch(err => {
+        throw err
+      })
+    } catch (e) {
+      next(e)
+    }
   }
 }
 
-HMProductSchema.pre('save', (next) => {
-  const currentDate = new Date
-
-  this.updated_at = currentDate
-  if (!this.created_at) 
-    this.created_at = currentDate
-
-  this.slug = uCommon.slugify(this.name)
-
-  next()
-})
+HMProductSchema.pre('save', HMProductSchema._middlewareFuncs.preSave)
+HMProductSchema.pre('update', HMProductSchema._middlewareFuncs.preUpdate)
+HMProductSchema.pre('findOneAndUpdate', HMProductSchema._middlewareFuncs.preUpdate)
+HMProductSchema.pre('remove', HMProductSchema._middlewareFuncs.preRemove)
+HMProductSchema.pre('findOneAndRemove', HMProductSchema._middlewareFuncs.preRemove)
 
 const HMProduct = mongoose.model('HMProduct', HMProductSchema)
 
