b8abff2a4419babf4e64b144f7780f5893be7553
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-20 23:52:15 -0600

    Refactoring schema, only code necessary, other fields will be populated in middleware

--- app/api/models/schemas/OrderProductSchema.js
+++ app/api/models/schemas/OrderProductSchema.js
@@ -2,31 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 module.exports = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
   code: { // Original ID
     type: Schema.Types.ObjectId,
     rel: 'Product',
     required: true
   },
-  quantity: {
-    type: Number,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
+  name: String,
+  quantity: Number,
+  price: Number,
   customs: [{
-    key: {
-      type: String,
-      required: true
-    },
-    value: {
-      type: String,
-      required: true
-    }
+    key: String,
+    value: String
   }]
 })
