5830eef1cd4f2a58e6bd57b02454dca09278c4a1
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-23 13:29:14 -0600

    Testing create methid

--- app/api/controllers/tests/clientCtrl/ctrlSimple.test.js
+++ app/api/controllers/tests/clientCtrl/ctrlSimple.test.js
@@ -1,18 +1,86 @@
-const clientCtrl = require('../../customFieldCtrl')
+const clientCtrl = require('../../clientCtrl')
+
+jest.mock('../../../models/Client')
+let Client = require('../../../models/Client')
+
+const uSchemas = require('../../../utils/validSchemas')
 
 describe('clientCtrl -> create', () => {
+  let clientToSend, savedClient
 
-  test('Should instantiate new Client with received body')
+  beforeEach(() => {
+    clientToSend = uSchemas.getValidClient()
+    savedClient  = uSchemas.getValidClient()
 
-  test('Should call save on the new Client')
+    Client.prototype.save = jest.fn(() => new Promise((resolve, reject) => { resolve(savedClient) }))
+  })
 
-  test('Should return the new Client after saved')
+  test('Should instantiate new Client with received body', async () => {
+    await clientCtrl.create(clientToSend)
 
-  test('Should throw a ValidationError')
+    expect(Client.mock.calls.length).toBe(1)
+    expect(Client.mock.calls[0][0]).toBe(clientToSend)
+  })
 
-  test('Should throw a DuplicatedError')
+  test('Should call save on the new Client', async () => {
+    await clientCtrl.create(clientToSend)
+    expect(Client.prototype.save.mock.calls.length).toBe(1)
+  })
 
-  test('Should throw a UnexpectedError')
+  test('Should return the new Client after saved', async () => {
+    const received = await clientCtrl.create(clientToSend)
+
+    expect(received).toBe(savedClient)
+  })
+
+  test('Should throw a ValidationError', done => {
+    Client.prototype.save = jest.fn(() => new Promise((resolve, reject) => {
+      const err = new Error('Faked Error')
+      err.name = 'ValidationError'
+      reject(err)
+    }))
+
+    clientCtrl.create(clientToSend).catch(err => {
+      expect(err.message).toBe('Faked Error')
+      expect(err.name).toBe('ValidationError')
+      expect(err.customMessage).toBe('Validation Error')
+      expect(err.customOrigin).toBe('Client')
+      done()
+    })
+  })
+
+  test('Should throw a DuplicatedError', done => {
+    Client.prototype.save = jest.fn(() => new Promise((resolve, reject) => {
+      const err = new Error('Faked Error')
+      err.code = 11000
+      reject(err)
+    }))
+
+    clientCtrl.create(clientToSend).catch(err => {
+      expect(err.message).toBe('Faked Error')
+      expect(err.name).toBe('DuplicationError')
+      expect(err.customMessage).toBe('Duplicated Email')
+      expect(err.customOrigin).toBe('Client')
+      done()
+    })
+  })
+
+  test('Should throw a UnexpectedError', done => {
+    Client.prototype.save = jest.fn(() => new Promise((resolve, reject) => {
+      const err = new Error('Faked Error')
+      err.customOrigin = 'ACustomOrigin'
+      err.name = 'WhatAnError'
+      reject(err)
+    }))
+
+    clientCtrl.create(clientToSend).catch(err => {
+      expect(err.message).toBe('Faked Error')
+      expect(err.name).toBe('WhatAnError')
+      expect(err.customMessage).toBe('Unexpected Error')
+      expect(err.customOrigin).toBe('ACustomOrigin')
+      done()
+    })
+  })
 
 })
 
