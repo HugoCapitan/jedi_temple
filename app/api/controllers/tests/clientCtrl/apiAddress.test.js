723af3f002e400a4a9372df71e2c100c6151c390
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-24 21:48:43 -0600

    Implementing test cases for addressUpdate method in Client's api

--- app/api/controllers/tests/clientCtrl/apiAddress.test.js
+++ app/api/controllers/tests/clientCtrl/apiAddress.test.js
@@ -116,7 +116,102 @@ describe('clientCtrl -> apiAddress', () => {
 
   })
 
-  describe('-> apiUpdateAddress', () => {})
+  describe('-> apiUpdateAddress', () => {
+    let addressIdToSend, foundAddress, addressUpdate, updatedAddress
+
+    beforeEach(() => {
+      addressIdToSend = new ObjectId('fff000fff000fff000fff000')
+      foundAddress = uSchemas.getValidAddress()
+      addressUpdate = { email: 'new@email.com' }
+      updatedAddress = Object.assign({}, foundAddress, addressUpdate)
+
+      foundClient.addresses = [...foundClient.addresses, foundAddress]
+      foundClient.addresses.find = jest.fn(() => foundAddress)
+
+      req.params.address_id = addressIdToSend
+      req.body = addressUpdate
+    })
+
+    test('Should call Client.findById with the sent client_id', async () => {
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(Client.findById.mock.calls.length).toBe(1)
+      expect(Client.findById.mock.calls[0][0]).toBe(clientIdToSend)
+    })
+  
+    test('Should call find on foundClient.addresses with the sent address_id', async() => {
+      const expectedQuery = { _id: addressIdToSend }
+
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(foundClient.addresses.find.mock.calls.length).toBe(1)
+      expect(foundClient.addresses.find.mock.calls[0][0]).toBe(expectedQuery)
+    })
+
+    test('Should update the desired address', async () => {
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(foundAddress).toEqual(updatedAddress)
+    })
+
+    test('Should call save on the foundClient', async () => {
+      await clientCtrl.apiUpdateAddress(req, res)
+      
+      expect(foundClient.save.mock.calls.length).toBe(1)
+    })
+
+    test('Should return the saved client', async () => {
+      await clientCtrl.apiUpdateAddress(req, res)
+      
+      expect(res.status).toBe(200)
+      expect(res.data).toBe(foundClient)
+    })
+
+    test('Should send a Client NotFoundError', async () => {
+      Client.findById = jest.fn(() => ({
+        exec: () => new Promise((resolve, reject) => { resolve(null) })
+      }))
+
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(res.statusCode).toBe(404)
+      expect(res.data).toBe(`Client with id: ${clientIdToSend}, not found`)
+    })
+
+    test('Should send a Address NotFoundError', async () => {
+      foundClient.addresses.find(() => null)
+
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(res.statusCode).toBe(404)
+      expect(res.data).toBe(`Address with id: ${addressIdToSend}, not found for client with id: ${clientIdToSend}`)
+    })
+
+    test('Should throw a ValidationError', async () => {
+      foundClient.save = jest.fn(() => new Promise((resolve, reject) => {
+        const err = new Error('Faked Error')
+        err.name = 'ValidationError'
+        reject(err)
+      }))
+
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(res.statusCode).toBe(403)
+      expect(res.data).toBe('Validation Error')
+    })
+
+    test('Should send a UnexpectedError', async () => {
+      foundClient.save = jest.fn(() => new Promise((resolve, reject) => { 
+        reject(new Error('Faked Error')) 
+      }))
+
+      await clientCtrl.apiUpdateAddress(req, res)
+
+      expect(res.statusCode).toBe(500)
+      expect(res.data).toBe('Unexpected Error')
+    })
+
+  })
 
   describe('-> apiRemoveAddress', () => {})
 
