2dd828d10aae29da83719081a353320c9fac0a27
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-25 12:20:44 -0600

    Adding missing client routes

--- app/api/routes/client.js
+++ app/api/routes/client.js
@@ -9,9 +9,17 @@ module.exports = app => {
     .put(clientCtrl.apiUpdate)
     .delete(clientCtrl.apiRemove)
 
-  app.route('/client/:client_id/addresses/')
-    .post(clientCtrl.apiCreateAddress)
+  app.post('/client/:client_id/addresses/', clientCtrl.apiCreateAddress)
   app.route('/client/:client_id/addresses/:address_id')
     .put(clientCtrl.apiUpdateAddress)
     .delete(clientCtrl.apiRemoveAddress)
+
+  app.post('/client/:client_id/orders/:order_id', clientCtrl.apiAddOrder)
+  app.delete('/client/:client_id/orders/:order_id', clientCtrl.apiRemoveOrder)
+
+  app.post('/client/:client_id/reservations/:reservation_id', clientCtrl.apiAddReservation)
+  app.delete('/client/:client_id/reservations/:reservation_id', clientCtrl.apiRemoveReservation)
+
+  app.post('/client/:client_id/wishlist/:wish_id', clientCtrl.apiAddWish)
+  app.delete('/client/:client_id/wishlist/:wish_id', clientCtrl.apiRemoveWish)
 }
