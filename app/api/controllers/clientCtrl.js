a4f637fef6387d30032196002565d818228e86af
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-25 11:11:28 -0600

    addReservation method implemented

--- app/api/controllers/clientCtrl.js
+++ app/api/controllers/clientCtrl.js
@@ -253,7 +253,25 @@ async function apiAddOrder(req, res) {
 }
 
 async function apiAddReservation(req, res) {
+  try {
+    const clientToUpdate = await Client.findById(req.params.client_id).exec()
+    if (!clientToUpdate) {
+      const notFoundError = new Error(`Client with id: ${req.params.client_id}, not found`)
+      notFoundError.name = "NotFoundError"
+      throw notFoundError
+    }
 
+    clientToUpdate.reservations.push(req.params.reservation_id)
+    await clientToUpdate.save()
+    res.status(200).json(clientToUpdate)
+  } catch (e) {
+    if (e.name === 'NotFoundError')
+      sendError(404, e.message, e, res)
+    else if (e.name === 'ValidationError')
+      sendError(403, 'Validation Error', e, res)
+    else
+      sendError(500, 'Unexpected Error', e, res)
+  }
 }
 
 async function apiAddWish(req, res) {
