8b23a8c558c4cc6a6e2270c60c00cd6c8010cbe8
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-27 11:46:16 -0600

    Tests for CustomFieldCtrl.apiCreate

--- app/api/controllers/tests/customFieldCtrl/api.test.js
+++ app/api/controllers/tests/customFieldCtrl/api.test.js
@@ -65,4 +65,74 @@ describe('customFieldCtrl -> API', () => {
 
   })
 
+  describe('apiCreate', () => {
+    let customFieldToSend, savedCustomField
+
+    beforeEach(() => {
+      customFieldToSend = uSchemas.getValidNumberCustom()
+      savedCustomField  = uSchemas.getValidNumberCustom()
+      req.body = customFieldToSend
+      CustomField.prototype.save = jest.fn(() => new Promise((resolve, reject) => {
+        resolve(savedCustomField)
+      }))
+    })
+
+    test('Should instantiate a CustomField with the sent body', async () => {
+      await customFieldCtrl.apiCreate(req, res)
+
+      expect(CustomField.mock.calls.length).toBe(1)
+      expect(CustomField.mock.calls[0][0]).toBe(customFieldToSend)
+    })
+
+    test('Should call CustomField.prototype.save', async () => {
+      await customFieldCtrl.apiCreate(req, res)
+      expect(CustomField.prototype.save.mock.calls.length).toBe(1)
+    })
+
+    test('Should send back the saved CustomField', async () => {
+      await customFieldCtrl.apiCreate(req, res)
+  
+      expect(res.statusCode).toBe(200)
+      expect(res.data).toEqual(savedCustomField)
+    })
+
+    test('Should send a ValidationError', async () => {
+      CustomField.prototype.save = jest.fn(prod => new Promise((resolve, reject) => {
+        const valError = new Error('Faked Error')
+        valError.name = "ValidationError"
+        reject(valError)
+      }))
+  
+      await customFieldCtrl.apiCreate(req, res)
+  
+      expect(res.statusCode).toBe(403)
+      expect(res.data).toBe("Validation Error")
+    })
+
+    test('Should send a DuplicationError', async () => {
+      CustomField.prototype.save = jest.fn(prod => new Promise((resolve, reject) => {
+        const dupError = new Error('Faked Error')
+        dupError.code = 11000
+        reject(dupError)
+      }))
+  
+      await customFieldCtrl.apiCreate(req, res)
+  
+      expect(res.statusCode).toBe(409)
+      expect(res.data).toBe('Duplicated Name')
+    })
+
+    test('Should send a UnexpectedError', async () => {
+      CustomField.prototype.save = jest.fn(prod => new Promise((resolve, reject) => {
+        reject(new Error('Faked Error'))
+      }))
+  
+      await customFieldCtrl.apiCreate(req, res)
+  
+      expect(res.statusCode).toBe(500)
+      expect(res.data).toBe('Unexpected Error')
+    })
+
+  })
+
 })
