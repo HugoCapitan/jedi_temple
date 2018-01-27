cc759b31e6f36d5bcf8fca8588ed8b63de7cee0e
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-27 11:33:19 -0600

    new test files for customFieldCtrl

---
+++ app/api/controllers/tests/customFieldCtrl/api.test.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
+
+const customFieldCtrl = require('../../customFieldCtrl')
+
+jest.mock('../../../models/CustomField')
+const CustomField = require('../../../models/CustomField')
+
+const uSchemas = require('../../../utils/validSchemas')
+
+describe('customFieldCtrl -> API', () => {
+
+  
+
+})
