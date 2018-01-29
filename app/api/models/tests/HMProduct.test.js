64323d326367152eea05075c2518bd38f019beb4
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-28 22:14:36 -0600

    Tests for preSave validation ehehe

--- app/api/models/tests/HMProduct.test.js
+++ app/api/models/tests/HMProduct.test.js
@@ -1,4 +1,6 @@
 const moment = require('moment')
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
 
 const HMProduct = require('../HMProduct')
 jest.mock('../Store')
@@ -32,25 +34,45 @@ describe('HMProduct Model', () => {
 
     test('Should be invalid if material missing price or name', () => {
       const malformedMaterial = uSchemas.getValidHMProduct()
-      malformedMaterial.materials[0].price = undefined
-      malformedMaterial.materials[0].name = undefined
+      malformedMaterial.materials[0].material_name = undefined
+      malformedMaterial.materials[0].material_price = undefined
 
       const m = new HMProduct( malformedMaterial )
       const v = m.validateSync()
 
       expect(uCommon.howManyKeys(v.errors)).toBe(2)
-      expect(v.errors['materials.0.name']).toBeTruthy()
-      expect(v.errors['materials.0.price']).toBeTruthy()
+      expect(v.errors['materials.0.material_name']).toBeTruthy()
+      expect(v.errors['materials.0.material_price']).toBeTruthy()
+    })
+
+  })
+
+  describe('Model Subschema', () => {
+  
+    test('Should be invalid if missing model_name or material_id', () => {
+      const malformedModel = Object.assign(uSchemas.getValidHMProduct(), { models: [ {} ] })
+
+      const m = new HMProduct(malformedModel)
+      const v = m.validateSync()
+
+      expect( uCommon.howManyKeys(v.errors) ).toBe(2)
+      expect(v.errors['models.0.model_name']).toBeTruthy()
+      expect(v.errors['models.0.material_id']).toBeTruthy()
     })
 
   })
 
   describe('preSave Middleware', () => {
+    let validHMProduct
     const bindMiddleware = context => 
       HMProduct.schema._middlewareFuncs.preSave.bind(context)
 
+    beforeEach(() => {
+      validHMProduct = uSchemas.getValidHMProduct()
+    })
+
     test('Should call next', done => {
-      const context = { name: 'bracelet' }
+      const context = validHMProduct
       const boundMiddleware = bindMiddleware(context)
       const next = err => {
         expect(err).toBeFalsy()
@@ -61,7 +83,7 @@ describe('HMProduct Model', () => {
     })
 
     test('Should add created_at and updated_at', done => {
-      const context = { name: 'bracelet' }
+      const context = validHMProduct
       const boundMiddleware = bindMiddleware(context)
       const next = err => {
         expect(err).toBeFalsy()
@@ -75,11 +97,11 @@ describe('HMProduct Model', () => {
 
     test('Should modify updated_at but not created_at', done => {
       const creationDate = moment().subtract(1, 'weeks').toDate()
-      const context = { name: 'bracelet', created_at: creationDate, updated_at: creationDate }
+      const context = { ...validHMProduct, created_at: creationDate, updated_at: creationDate }
       const boundMiddleware = bindMiddleware(context)
       const next = err => {
         expect(err).toBeFalsy()
-        expect( context.created_at ).toBe(creationDate)
+        expect(context.created_at).toBe(creationDate)
         expect( uValid.isThisMinute(context.updated_at) ).toBeTruthy()
         done()
       }
@@ -87,45 +109,81 @@ describe('HMProduct Model', () => {
       boundMiddleware(next)
     })
 
-    test('Materials should iterate materials for duplicates and call next with error', done => {
+    test('Should iterate materials for duplicates and call next with error', done => {
       const context = { 
-        name: 'canastita', 
+        ...validHMProduct, 
         materials: [{ 
-          name: 'olakease',
-          models: []
+          material_name: 'olakease'
         }, { 
-          name: 'hola que haces',
-          models: [] 
+          material_name: 'hola que haces'
         }, { 
-          name: 'olakease',
-          models: []
+          material_name: 'olakease'
         }] 
       }
       const boundMiddleware = bindMiddleware(context)
       const next = err => {
+        expect(err.message).toBe(`Duplicated value for materials in ${validHMProduct.name} HMProduct`)
         expect(err.name).toBe('ValidationError')
-        expect(err.message).toBe('Duplicated value for materials in canastita HMProduct')
         done()
       }
       
       boundMiddleware(next)
     })
 
-    test('Should iterate on every material\'s models for duplicates and call next with error', done => {
-      const context = { 
-        name: 'canastita', 
-        materials: [{
-          name: 'olakease',
-          models: ['nadanada', 'mucho', 'nadanada']
-        }, { 
-          name: 'hola que haces',
-          models: [] 
-        }] 
+    test('Should iterate models and remove model if wrong material_id', done => {
+      const context = validHMProduct
+      context.materials[0]._id = new ObjectId('ffffffffffffffffffffffff')
+
+      const boundMiddleware = bindMiddleware(context)
+      const next = err => {
+        expect(err).toBeFalsy()
+        expect(context.models.length).toBe(0)
+        done()
+      }
+
+      boundMiddleware(next)
+    })
+
+    test('Should iterate models and remove model if wrong material_id', done => {
+      const context = validHMProduct
+      context.materials[0]._id = new ObjectId('ffffffffffffffffffffffff')
+      context.models.push({ model_name: 'hehe modelo rarito', material_id: context.materials[0]._id })
+      context.models.push({ model_name: 'hehe otro rarito', material_id: new ObjectId('a0a0a0a0a0a0a0a0a00a0a0a') })
+
+      const boundMiddleware = bindMiddleware(context)
+      const next = err => {
+        expect(err).toBeFalsy()
+        expect(context.models.length).toBe(1)
+        expect(context.models[0].model_name).toBe('hehe otro rarito')
+        done()
+      }
+
+      boundMiddleware(next)
+    })
+
+    test('Should iterate models and return error if duplications for same material', done => {
+      newMatId = new ObjectId('affaffaffaffaffaffaffaff')
+      validHMProduct.materials.push({ 
+        _id: newMatId,
+        material_name: 'some fcking name',
+        material_price: 459.99 
+      })
+      const context = {
+        ...validHMProduct,
+        models: [{
+          model_name: 'superduper', material_id: validHMProduct.materials[0]._id
+        },{
+          model_name: 'superduper', material_id: validHMProduct.materials[1]._id
+        },{
+          model_name: 'hehehe', material_id: validHMProduct.materials[0]._id
+        },{
+          model_name: 'superduper', material_id: validHMProduct.materials[0]._id // <- Duplication 
+        }]
       }
       const boundMiddleware = bindMiddleware(context)
       const next = err => {
+        expect(err.message).toBe(`Duplicated value for model in ${validHMProduct.name} HMProduct`)
         expect(err.name).toBe('ValidationError')
-        expect(err.message).toBe('Duplicated value for models in material olakease in canastita HMProduct')
         done()
       }
 
