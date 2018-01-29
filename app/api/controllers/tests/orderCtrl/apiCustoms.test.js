5f8bf360a10071e8210d5c82d08e452795d16d83
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-29 17:41:54 -0600

    Finished writing test cases descriptions

--- app/api/controllers/tests/orderCtrl/apiCustoms.test.js
+++ app/api/controllers/tests/orderCtrl/apiCustoms.test.js
@@ -10,13 +10,75 @@ const uSchemas = require('../../../utils/validSchemas')
 
 describe('orderCtrl -> api custom methods', () => {
 
-  describe('updateShippingAddress', () => {})
+  describe('updateBillingAddress', () => {
 
-  describe('updateBillingAddress', () => {})
+    test('Should call Order.findById with sent order_id')
+
+    test('Should update foundOrder.billingAddress')
+
+    test('Should call foundOrder.save')
+
+    test('Should return updated Order')
+
+    test('Should return NotFoundError')
+
+    test('Should return ValidationError')
+
+    test('Should return UnexpectedError')
+
+  })
+
+  describe('updateShippingAddress', () => {
+
+    test('Should call Order.findById with sent order_id')
+
+    test('Should update foundOrder.shippingAddress')
+
+    test('Should call foundOrder.save')
+
+    test('Should return updated Order')
+
+    test('Should return NotFoundError')
+
+    test('Should return ValidationError')
+
+    test('Should return UnexpectedError')
+
+  })
 
 
-  describe('addProduct', () => {})
+  describe('addProduct', () => {
 
-  describe('removeProduct', () => {})
+    test('Should call Order.findById')
+
+    test('Should push product id to foundOrder.products')
+
+    test('Should call foundOrder.save')
+
+    test('Should return updated Order')
+
+    test('Should return NotFoundError')
+
+    test('Should return ValidationError')
+
+    test('Should return UnexpectedError')
+
+  })
+
+  describe('removeProduct', () => {
+
+    test('Should call Order.findById')
+
+    test('Should pull product id from foundOrder.products')
+
+    test('Should call foundOrder.save')
+
+    test('Should return updated Order')
+
+    test('Should return NotFoundError')
+
+    test('Should return UnexpectedError')
+
+  })
 
 })
