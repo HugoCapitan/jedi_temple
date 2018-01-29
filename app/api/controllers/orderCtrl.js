3d0d8b22b0ce3be7d96f3ba35be2f348e3de5477
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-29 12:05:50 -0600

    Fixing order api methods

--- app/api/controllers/orderCtrl.js
+++ app/api/controllers/orderCtrl.js
@@ -73,7 +73,7 @@ async function update(id, newOrder) {
 
 async function apiAll(req, res) {
   try {
-    let all = await Order.find()
+    let all = await Order.find().exec()
     res.status(200).json(all)
   } catch (e) {
     sendError(500, 'Unexpected Error', e, res)
@@ -96,9 +96,9 @@ async function apiCreate(req, res) {
 
 async function apiRead(req, res) {
   try {
-    const foundOrder = await Order.findById(req.params.id)
+    const foundOrder = await Order.findById(req.params.id).exec()
     if (!foundOrder) {
-      let notFoundError = new Error(`Order ${req.params.id} not found`)
+      let notFoundError = new Error(`Order with id: ${req.params.id}, not found`)
       notFoundError.name = 'NotFoundError'
       throw notFoundError
     }
@@ -106,7 +106,7 @@ async function apiRead(req, res) {
     res.status(200).json(foundOrder)
   } catch(e) {
     if (e.name === 'NotFoundError')
-      sendError(404, `Order ${req.params.id} not found`, e, res)
+      sendError(404, e.message, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -114,18 +114,12 @@ async function apiRead(req, res) {
 
 async function apiRemove(req, res) {
   try {
-    const removedOrder = await Order.findByIdAndRemove(req.params.id)
+    const removedOrder = await Order.findByIdAndRemove(req.params.id).exec()
 
-    if (!removedOrder) {
-      let notFoundError = new Error(`Order ${req.params.id} not found`)
-      notFoundError.name = "NotFoundError"
-      throw notFoundError
-    }
-
-    res.status(200).send(`Order ${removedOrder._id} deleted`)
+    res.status(200).json(removedOrder)
   } catch (e) {
     if (e.name === 'CastError' || e.name === 'NotFoundError')
-      sendError(404, `Order ${req.params.id} not found`, e, res)
+      sendError(404, `Order with id: ${req.params.id}, not found`, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -133,15 +127,14 @@ async function apiRemove(req, res) {
 
 async function apiUpdate(req, res) {
   try {
-    await Order.findByIdAndUpdate(req.params.id, req.body)
-    const updatedOrder = await Order.findById(req.params.id)
+    const updatedOrder = await Order.findByIdAndUpdate(req.params.id, req.body, {new: true}).exec()
 
     res.status(200).json(updatedOrder)
   } catch (e) {
     if (e.name === 'ValidationError')
       sendError(403, 'Validation Error', e, res)
     else if (e.name === 'CastError')
-      sendError(404, `Order ${req.params.id} not found`, e, res)
+      sendError(404, `Order with id: ${req.params.id}, not found`, e, res)
     else if (e.code === 11000)
       sendError(409, 'Duplicated Name', e, res)
     else
