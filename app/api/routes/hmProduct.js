840a85e7c15d0cb44f4b841c9f7a88be5f6f7525
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-29 12:48:02 -0600

    hmProduct routes

--- app/api/routes/hmProduct.js
+++ app/api/routes/hmProduct.js
@@ -8,4 +8,16 @@ module.exports = app => {
     .get(hmProductCtrl.apiRead)
     .put(hmProductCtrl.apiUpdate)
     .delete(hmProductCtrl.apiRemove)
+
+  app.route('/hm_product/:hmproduct_id/materials/')
+    .post(hmProductCtrl.apiCreateMaterial)
+  app.route('/hm_product/:hmproduct_id/materials/:material_id')
+    .put(hmProductCtrl.apiUpdateMaterial)
+    .delete(hmProductCtrl.apiRemoveMaterial)
+
+  app.route('/hm_product/:hmproduct_id/models/')
+    .post(hmProductCtrl.apiCreateModel)
+  app.route('/hm_product/:hmproduct_id/models/:model_id')
+    .put(hmProductCtrl.apiUpdateModel)
+    .delete(hmProductCtrl.apiRemoveModel)
 }
