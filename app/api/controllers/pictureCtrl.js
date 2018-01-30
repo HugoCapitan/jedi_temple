5e943b426ea1149010b4c76f385e4a0f58120c31
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-29 19:33:45 -0600

    Fixing api issues

--- app/api/controllers/pictureCtrl.js
+++ app/api/controllers/pictureCtrl.js
@@ -11,7 +11,7 @@ module.exports = {apiAll,
 
 async function apiAll(req, res) {
   try {
-    let all = await Picture.find()
+    let all = await Picture.find().exec()
     res.status(200).json(all)
   } catch (e) {
     sendError(500, 'Unexpected Error', e, res)
@@ -34,9 +34,9 @@ async function apiCreate(req, res) {
 
 async function apiRead(req, res) {
   try {
-    const foundPicture = await Picture.findById(req.params.id)
+    const foundPicture = await Picture.findById(req.params.id).exec()
     if (!foundPicture) {
-      let notFoundError = new Error(`Picture ${req.params.id} not found`)
+      let notFoundError = new Error(`Picture with id: ${req.params.id}, not found`)
       notFoundError.name = 'NotFoundError'
       throw notFoundError
     }
@@ -44,7 +44,7 @@ async function apiRead(req, res) {
     res.status(200).json(foundPicture)
   } catch(e) {
     if (e.name === 'NotFoundError')
-      sendError(404, `Picture ${req.params.id} not found`, e, res)
+      sendError(404, e.message, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -52,18 +52,18 @@ async function apiRead(req, res) {
 
 async function apiRemove(req, res) {
   try {
-    const removedPicture = await Picture.findByIdAndRemove(req.params.id)
+    const removedPicture = await Picture.findByIdAndRemove(req.params.id).exec()
 
     if (!removedPicture) {
-      let notFoundError = new Error(`Picture ${req.params.id} not found`)
+      let notFoundError = new Error(`Picture with id: ${req.params.id}, not found`)
       notFoundError.name = "NotFoundError"
       throw notFoundError
     }
 
-    res.status(200).send(`Picture ${removedPicture._id} deleted`)
+    res.status(200).send(removedPicture)
   } catch (e) {
     if (e.name === 'CastError' || e.name === 'NotFoundError')
-      sendError(404, `Picture ${req.params.id} not found`, e, res)
+      sendError(404, `Picture with id: ${req.params.id}, not found`, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -71,15 +71,14 @@ async function apiRemove(req, res) {
 
 async function apiUpdate(req, res) {
   try {
-    await Picture.findByIdAndUpdate(req.params.id, req.body)
-    const updatedPicture = await Picture.findById(req.params.id)
+    const updatedPicture = await Picture.findByIdAndUpdate(req.params.id, req.body, {new: true}).exec()
 
     res.status(200).json(updatedPicture)
   } catch (e) {
     if (e.name === 'ValidationError')
       sendError(403, 'Validation Error', e, res)
     else if (e.name === 'CastError')
-      sendError(404, `Picture ${req.params.id} not found`, e, res)
+      sendError(404, `Picture with id: ${req.params.id}, not found`, e, res)
     else if (e.code === 11000)
       sendError(409, 'Duplicated Name', e, res)
     else
