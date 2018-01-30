559e8ad8993cc9a320608f7679833df669ff4cfd
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-29 19:43:30 -0600

    fixing productCtrl.api issueas

--- app/api/controllers/productCtrl.js
+++ app/api/controllers/productCtrl.js
@@ -12,7 +12,7 @@ module.exports = {
 
 async function apiAll(req, res) {
   try {
-    let all = await Product.find()
+    let all = await Product.find().exec()
     res.status(200).json(all)
   } catch (e) {
     sendError(500, 'Unexpected Error', e, res)
@@ -35,9 +35,9 @@ async function apiCreate(req, res) {
 
 async function apiRead(req, res) {
   try {
-    const foundProduct = await Product.findById(req.params.id)
+    const foundProduct = await Product.findById(req.params.id).exec()
     if (!foundProduct) {
-      let notFoundError = new Error(`Product ${req.params.id} not found`)
+      let notFoundError = new Error(`Product with id: ${req.params.id}, not found`)
       notFoundError.name = 'NotFoundError'
       throw notFoundError
     }
@@ -45,7 +45,7 @@ async function apiRead(req, res) {
     res.status(200).json(foundProduct)
   } catch(e) {
     if (e.name === 'NotFoundError')
-      sendError(404, `Product ${req.params.id} not found`, e, res)
+      sendError(404, e.message, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -53,18 +53,18 @@ async function apiRead(req, res) {
 
 async function apiRemove(req, res) {
   try {
-    const removedProduct = await Product.findByIdAndRemove(req.params.id)
+    const removedProduct = await Product.findByIdAndRemove(req.params.id).exec()
 
     if (!removedProduct) {
-      let notFoundError = new Error(`Product ${req.params.id} not found`)
+      let notFoundError = new Error(`Product with id: ${req.params.id}, not found`)
       notFoundError.name = "NotFoundError"
       throw notFoundError
     }
 
-    res.status(200).send(`Product ${removedProduct._id} deleted`)
+    res.status(200).send(removedProduct)
   } catch (e) {
     if (e.name === 'CastError' || e.name === 'NotFoundError')
-      sendError(404, `Product ${req.params.id} not found`, e, res)
+      sendError(404, `Product with id: ${req.params.id}, not found`, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -72,15 +72,13 @@ async function apiRemove(req, res) {
 
 async function apiUpdate(req, res) {
   try {
-    await Product.findByIdAndUpdate(req.params.id, req.body)
-    const updatedProduct = await Product.findById(req.params.id)
-
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true}).exec()
     res.status(200).json(updatedProduct)
   } catch (e) {
     if (e.name === 'ValidationError')
       sendError(403, 'Validation Error', e, res)
     else if (e.name === 'CastError')
-      sendError(404, `Product ${req.params.id} not found`, e, res)
+      sendError(404, `Product with id: ${req.params.id}, not found`, e, res)
     else if (e.code === 11000)
       sendError(409, 'Duplicated Name', e, res)
     else
