e6272a918012a438196a8aa87d6cbd75ab1bf93c
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-31 18:18:23 -0600

    Writing Reservation's preSave tests

--- app/api/models/tests/Reservation.test.js
+++ app/api/models/tests/Reservation.test.js
@@ -1,13 +1,17 @@
-const Reservation = require('../Reservation')
 const moment = require('moment')
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
 
-const { howManyKeys } = require('../../utils')
-const { getValidReservation } = require('../../utils/validSchemas')
+const Reservation = require('../Reservation')
+
+const uCommon = require('../../utils')
+const uSchemas = require('../../utils/validSchemas')
+const uValid = require('../../utils/validators')
 
 describe('Reservation model', () => {
 
   test('Should be valid', () => {
-    const m = new Reservation( getValidReservation() )
+    const m = new Reservation( uSchemas.getValidReservation() )
     const v = m.validateSync()
 
     expect(v).toBeFalsy()
@@ -17,7 +21,7 @@ describe('Reservation model', () => {
     const m = new Reservation({ })
     const v = m.validateSync()
 
-    expect( howManyKeys(v.errors) ).toBe(5)
+    expect( uCommon.howManyKeys(v.errors) ).toBe(5)
     expect(v.errors.email).toBeTruthy()
     expect(v.errors.status).toBeTruthy()
     expect(v.errors.arrive_date).toBeTruthy()
@@ -26,52 +30,93 @@ describe('Reservation model', () => {
   })
 
   test('Should be invalid if arriving or departure not dates', () => {
-    const m = new Reservation( Object.assign( getValidReservation(), { arrive_date: 'wrong', departure_date: 'alsowrong' } ) )
+    const m = new Reservation( Object.assign( uSchemas.getValidReservation(), { arrive_date: 'wrong', departure_date: 'alsowrong' } ) )
     const v = m.validateSync()
 
-    expect( howManyKeys(v.errors) ).toBe(2)
+    expect( uCommon.howManyKeys(v.errors) ).toBe(2)
     expect(v.errors.arrive_date).toBeTruthy()
     expect(v.errors.departure_date).toBeTruthy()
   })
 
   test('Should be invalid if arriving after departure', () => {
     const m = new Reservation( Object.assign( 
-      getValidReservation(), { 
+      uSchemas.getValidReservation(), { 
         arrive_date: moment().add('1', 'weeks').toDate(),
         departure_date: moment().add('1', 'days').toDate()
       })
     )
     const v = m.validateSync()
 
-    expect( howManyKeys(v.errors) ).toBe(2)
+    expect( uCommon.howManyKeys(v.errors) ).toBe(2)
     expect(v.errors.arrive_date).toBeTruthy()
     expect(v.errors.departure_date).toBeTruthy()
   })
 
   test('Should be invalid if email is not an email', () => {
-    const m = new Reservation( Object.assign( getValidReservation(), { email: 'whatwhat' } ) )
+    const m = new Reservation( Object.assign( uSchemas.getValidReservation(), { email: 'whatwhat' } ) )
     const v = m.validateSync()
 
-    expect( howManyKeys(v.errors) ).toBe(1)
+    expect( uCommon.howManyKeys(v.errors) ).toBe(1)
     expect(v.errors.email).toBeTruthy()
   })
 
   describe('preSave Middleware', () => {
 
-    const boundMiddleware = context => {
+    const bindMiddleware = context => {
       if (!context.isModified) context.isModified = prop => false
-      return Reservation._middlewareFuncs.preSave.bind(context)
+      return Reservation.schema._middlewareFuncs.preSave.bind(context)
     }
 
-    test('Should call next')
+    test('Should call next', done => {
+      const context = uSchemas.getValidReservation()
+      const boundMiddleware = bindMiddleware(context)
+      const next = err => {
+        expect(err).toBeFalsy()
+        done()
+      }
 
-    test('Should add dates')
+      boundMiddleware(next)
+    })
 
-    test('Should update update date')
+    test('Should add dates', done => {
+      const context = uSchemas.getValidReservation()
+      const boundMiddleware = bindMiddleware(context)
+      const next = err => {
+        expect(err).toBeFalsy()
+        expect( uValid.isThisMinute(context.created_at) ).toBeTruthy()
+        expect( uValid.isThisMinute(context.updated_at) ).toBeTruthy()
+        done()
+      }
 
-    test('Should calculate total')
+      boundMiddleware(next)
+    })
 
-    test('Should call next with UnexpectedError')
+    test('Should update update date', done => {
+      const lastWeek = moment().subtract(1, 'weeks').toDate()
+      const context = Object.assign(uSchemas.getValidReservation(), {
+        created_at: lastWeek, updated_at: lastWeek
+      })
+      const boundMiddleware = bindMiddleware(context)
+      const next = err => {
+        expect(err).toBeFalsy()
+        expect( uValid.isThisMinute(context.updated_at) ).toBeTruthy()
+        expect(context.created_at).toBe(lastWeek)
+        done()
+      }
+
+      boundMiddleware(next)
+    })
+
+    test('Should calculate total', done => {
+      const context = uSchemas.getValidReservation()
+      const total = moment(context.departure_date).diff(context.arrive_date, 'days') * context.night_price
+      const boundMiddleware = bindMiddleware(context)
+      const next = err => {
+        expect(err).toBeFalsy()
+        expect(context.total).toBe(total)
+        done()
+      }
+    })
 
   })
 
