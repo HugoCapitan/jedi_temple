2c2d46664516cbaaaad48c9cdf180021b7bc8e09
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-31 21:29:56 -0600

    Refactoring reservation tests

--- app/api/controllers/reservationCtrl.js
+++ app/api/controllers/reservationCtrl.js
@@ -7,12 +7,14 @@ module.exports = {
   apiCreate, 
   apiRead, 
   apiRemove, 
-  apiUpdate
+  apiUpdate,
+
+  apiUpdateDatesPrice
 }
 
 async function apiAll(req, res) {
   try {
-    let all = await Reservation.find()
+    let all = await Reservation.find().exec()
     res.status(200).json(all)
   } catch (e) {
     sendError(500, 'Unexpected Error', e, res)
@@ -35,9 +37,9 @@ async function apiCreate(req, res) {
 
 async function apiRead(req, res) {
   try {
-    const foundReservation = await Reservation.findById(req.params.id)
+    const foundReservation = await Reservation.findById(req.params.id).exec()
     if (!foundReservation) {
-      let notFoundError = new Error(`Reservation ${req.params.id} not found`)
+      let notFoundError = new Error(`Reservation with id: ${req.params.id}, not found`)
       notFoundError.name = 'NotFoundError'
       throw notFoundError
     }
@@ -45,7 +47,7 @@ async function apiRead(req, res) {
     res.status(200).json(foundReservation)
   } catch(e) {
     if (e.name === 'NotFoundError')
-      sendError(404, `Reservation ${req.params.id} not found`, e, res)
+      sendError(404, `Reservation with id: ${req.params.id}, not found`, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -53,10 +55,10 @@ async function apiRead(req, res) {
 
 async function apiRemove(req, res) {
   try {
-    const removedReservation = await Reservation.findByIdAndRemove(req.params.id)
+    const removedReservation = await Reservation.findByIdAndRemove(req.params.id).exec()
 
     if (!removedReservation) {
-      let notFoundError = new Error(`Reservation ${req.params.id} not found`)
+      let notFoundError = new Error(`Reservation with id: ${req.params.id}, not found`)
       notFoundError.name = "NotFoundError"
       throw notFoundError
     }
@@ -64,7 +66,7 @@ async function apiRemove(req, res) {
     res.status(200).send(`Reservation ${removedReservation._id} deleted`)
   } catch (e) {
     if (e.name === 'CastError' || e.name === 'NotFoundError')
-      sendError(404, `Reservation ${req.params.id} not found`, e, res)
+      sendError(404, `Reservation with id: ${req.params.id}, not found`, e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
   }
@@ -72,19 +74,39 @@ async function apiRemove(req, res) {
 
 async function apiUpdate(req, res) {
   try {
-    await Reservation.findByIdAndUpdate(req.params.id, req.body)
-    const updatedReservation = await Reservation.findById(req.params.id)
+    const updatedReservation = await Reservation.findByIdAndUpdate(req.params.id, req.body, {new:true}).exec()
 
     res.status(200).json(updatedReservation)
   } catch (e) {
     if (e.name === 'ValidationError')
       sendError(403, 'Validation Error', e, res)
     else if (e.name === 'CastError')
-      sendError(404, `Reservation ${req.params.id} not found`, e, res)
+      sendError(404, `Reservation with id: ${req.params.id}, not found`, e, res)
     else if (e.code === 11000)
       sendError(409, 'Duplicated Name', e, res)
     else
       sendError(500, 'Unexpected Error', e, res)
 
+  }
+}
+
+async function apiUpdateDatesPrice(req, res) {
+  try {
+    const reservationToUpdate = await Reservation.findById(req.params.id)
+    if (!reservationToUpdate) {
+      const err = new Error(`Reservation with id: ${req.params.id}, not found`)
+      err.name = 'NotFoundError'
+      throw err
+    }
+    Object.assign(reservationToUpdate, req.body)
+    reservationToUpdate.save()
+    res.status(200).json(reservationToUpdate)
+  } catch(err) {
+    if (err.name === 'ValidationError')
+      sendError(403, 'Validation Error', e, res)
+    else if (err.name === 'NotFoundError')
+      sendError(404, e.message, e, res)
+    else 
+      sendError(500, 'Unexpected Error', e, res)    
   }
 }
