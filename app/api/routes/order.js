9a598906620c0d1a605f9fa9c83be943dde01528
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-01-31 22:04:44 -0600

    new routes for order

--- app/api/routes/order.js
+++ app/api/routes/order.js
@@ -8,4 +8,10 @@ module.exports = app => {
     .get(orderCtrl.apiRead)
     .put(orderCtrl.apiUpdate)
     .delete(orderCtrl.apiRemove)
+
+  app.put('/order/:order_id/billing_address', orderCtrl.apiUpdateBillingAddress)
+  app.put('/order/:order_id/shipping_address', orderCtrl.apiUpdateShippingAddress)
+
+  app.post('/order/:order_id/products', orderCtrl.apiAddProduct)
+  app.delete('/order/:order_id/products/:product_id', orderCtrl.apiRemoveProduct)
 }
