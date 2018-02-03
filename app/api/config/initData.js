89cd126c819a5cf9d62aae45620de712bd34bd6f
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-03 10:02:19 -0600

    Init process for paypals web_experiences

--- app/api/config/initData.js
+++ app/api/config/initData.js
@@ -1,4 +1,7 @@
+const axios = require('axios')
+
 const CustomField = require('../models/CustomField')
+const paypalCtrl = require('../controllers/paypalCtrl')
 
 module.exports = async () => {
   try {
@@ -10,6 +13,32 @@ module.exports = async () => {
     if (!HMModelCustom) await saveHMModelCustom()
     if (!MaterialCustom) await saveMaterialCustom()
 
+    // get esperiences within this paypal app
+    const experiences = await paypalCtrl.getRemoteXps()
+    // reduce to get store experiences
+    const storesXps = experiences.reduce((acc, current) => {
+      if (current.name === 'TuchaDesigns Store Payment'
+       || current.name === 'Kampamocha Store Payment'
+       || current.name === 'Unahil Payment')
+        acc.push(current)
+
+      return acc
+    }, [])
+
+    // if stores not complete, destroy all and create all
+    const destroys = []
+    if (storesXps.length != 3) {
+      for (const xp of experiences) { destroys.push(ppConf.destroyXp()) }
+      await Promise.all(destroys)
+      await paypalCtrl.initXps()
+    } else {
+      for (const xp of experiences) {
+        if (xp.name === 'Kampamocha Store Payment') process.env.NODE_PP_KAMPA_XP = JSON.stringify(xp)
+        if (xp.name === 'TuchaDesigns Store Payment') process.env.NODE_PP_TUCHA_XP = JSON.stringify(xp)
+        if (xp.name === 'Unahil Payment') process.env.NODE_PP_UNAHIL_XP = JSON.stringify(xp)
+      }
+    }
+    
   } catch (e) {
     console.log('ERROR ON DATA INIT: ', e)
   }
