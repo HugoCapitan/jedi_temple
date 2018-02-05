e4e7ff3791cc7c899a7123011432fb9a795797bb
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-04 20:44:33 -0600

    First test casesfor ppController

---
+++ app/api/controllers/tests/paypalCtrl.test.js
@@ -0,0 +1,57 @@
+const paypalCtrl = require('../paypalCtrl')
+
+jest.mock('axios')
+const axios = require('axios')
+
+describe('paypalCtrl', () => {
+
+  describe('buildPaymentRequest', () => {
+
+    test('Should return a correct paypal payment obj')
+
+    test('Should return an error malformed paymentForm (method=paypal)')
+
+    test('Should return an error malformed paymentForm (method=credit_card)')
+    
+    test('Should return an error -> URLS not found')
+
+    test('Should return an error -> Experience not found')
+
+  })
+
+  describe('createPaymentEndpoint', () => {
+
+    test('Should call get token')
+
+    test('Should call buildPayment')
+
+    test('Should call axios with correct method and options')
+
+    test('Should check on response for redirect url and cal res.redirect (paypal)')
+
+    test('Should check on response for redirect url and cal res.redirect (credit_card)')
+
+    test('Should send axios response error correctly formatted')
+
+    test('Should send axios request error correctly formatted')
+
+    test('Should send error returned by builPaymentRequest')
+
+    test('Should send unexpectedError')
+
+  })
+  
+  describe('getAuthToken', () => {
+
+  })
+
+  describe('getLocalXps', () => {
+    
+  })
+  
+  describe('getRemoteExperiences', () => {
+    
+  })
+  
+
+})
