9e82c3b1b0675db34f0a13cb26298d33fb1c1cea
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-04 21:02:24 -0600

    first test cases for endpoints tests

---
+++ app/api/controllers/tests/paypalCtrl/paypalEndpoints.test.js
@@ -0,0 +1,45 @@
+const paypalCtrl = require('../../paypalCtrl')
+
+describe('paypalCtrl Endpoints', () => {
+  
+  describe('createPaymentEndpoint', () => {
+    
+    test('Should call core function')
+    
+    test('Should respond (200) with call function\'s return')
+
+    test('Should respond (500) with call function\'s error')
+
+  })
+
+  describe('getAuthTokenEndpoint', () => {
+
+    test('Should call core function')
+    
+    test('Should respond (200) with call function\'s return')
+
+    test('Should respond (500) with call function\'s error')
+  
+  })
+  
+  describe('initExperiencesEndpoint', () => {
+    
+    test('Should call core function')
+    
+    test('Should respond (200) with call function\'s return')
+
+    test('Should respond (500) with call function\'s error')
+
+  })
+  
+  describe('getLocalExperiencesEndpoint', () => {
+    
+    test('Should call core function')
+    
+    test('Should respond (200) with call function\'s return')
+
+    test('Should respond (500) with call function\'s error')
+
+  })
+  
+})
