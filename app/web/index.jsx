8110cf7f54e93a77c5ca5fedc9ac72993f43ac4a
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-13 17:03:23 -0600

    First setup for app

--- app/web/index.jsx
+++ app/web/index.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+
 import App from './components/App'
+import { setAuthToken } from './actions'
+import configureStore from './configureStore'
 
+const store = configureStore()
 const token = document.getElementById('root').getAttribute('token')
+store.dispatch(setAuthToken(token))
 
-render(
-  <App />,
-  document.getElementById('root')
-)
+const renderApp = () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    render(
-      <App />,
-      document.getElementById('root')
-    )
+    renderApp()
   })
 }
+
+renderApp()
