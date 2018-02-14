7b6ec2fb183d829bade8f609519336e71ede846d
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-14 14:17:45 -0600

    added missing file for reducers0 0

--- app/web/constants/index.js
+++ app/web/constants/index.js
@@ -1,9 +1,17 @@
-
+/**************
+ * AUTH TOKEN *
+ **************/
 export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN'
 
+/**************
+ * UI ACTIONS *
+ **************/
 export const TOGGLE_DRAWER = 'TOGGLE_DRAWER'
 export const CHANGE_DRAWER_OPEN = 'CHANGE_DRAWER_OPEN'
 
+/***********
+ * NETWORK *
+ ***********/
 export const REQUEST_PRODUCTS = 'REQUEST_PRODUCTS'
 export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
 export const FAILED_REQUEST   = 'FAILED_REQUEST'
