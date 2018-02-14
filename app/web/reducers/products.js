f3110d8eaa565206f8f0a15265f2591d95bcf792
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-14 13:38:58 -0600

    Reducers for async actions

--- app/web/reducers/products.js
+++ app/web/reducers/products.js
@@ -1,7 +1,16 @@
+import { REQUEST_PRODUCTS, RECEIVE_PRODUCTS } from '../constants'
 
+const initialState = {
+  isFetching: false,
+  items: {}
+}
 
-function products (state = [], action) {
+function products (state = initialState, action) {
   switch (action.type) {
+    case REQUEST_PRODUCTS:
+      return {...state, isFetching: true}
+    case RECEIVE_PRODUCTS: 
+      return {...state, items: action.products, isFetching: false}
     default:
       return state
   }
--- app/web/reducers/ui.js
+++ app/web/reducers/ui.js
@@ -1,18 +1,21 @@
-import { TOGGLE_DRAWER, CHANGE_DRAWER_OPEN } from '../constants'
+import { TOGGLE_DRAWER, CHANGE_DRAWER_OPEN, FAILED_REQUEST } from '../constants'
 
 const initialState = {
-  drawerOpen: false
+  drawerOpen: false,
+  fetchingError: null
 }
 
-const uiDrawer = (state = initialState, action) => {
+const ui = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_DRAWER: 
       return { ...state, drawerOpen: !state.drawerOpenopen }
     case CHANGE_DRAWER_OPEN: 
       return { ...state, drawerOpen: action.open }
+    case FAILED_REQUEST: 
+      return { ...state, fetchingError: action.error }
     default:
       return state
   }
 }
 
-export default uiDrawer
+export default ui
