962bac32642f763e6b2484b6c477be688a404181
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-14 12:26:59 -0600

    Finished ProductListHeader

--- app/web/components/ProductListHeader.jsx
+++ app/web/components/ProductListHeader.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar'
+import IconEditorModeEdit from  'material-ui/svg-icons/editor/mode-edit'
+import IconContentAddCircle from 'material-ui/svg-icons/content/add-circle-outline'
+import IconButton from 'material-ui/IconButton'
+import FontIcon from 'material-ui/FontIcon'
 
 const ProductListHeader = ({ title, onEdit, onAdd }) => (
   <Toolbar>
     <ToolbarGroup>
       <ToolbarTitle text={title} />
     </ToolbarGroup>
+    <ToolbarGroup lastChild={true}>
+      <IconButton touch={true} onClick={onEdit}>
+        <IconEditorModeEdit />
+      </IconButton>
+      <IconButton touch={true} onClick={onAdd}>
+        <IconContentAddCircle />
+      </IconButton>
+    </ToolbarGroup>        
   </Toolbar>
 )
 
