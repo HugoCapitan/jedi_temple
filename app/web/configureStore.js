db188bef22d599867e6c26c327f73984e53d351c
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-14 13:26:45 -0600

    Installing and using middleware for async actions and logs

--- app/web/configureStore.js
+++ app/web/configureStore.js
@@ -1,8 +1,18 @@
-import { createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
 import adminApp from './reducers'
 
+const loggerMiddleware = createLogger()
+
 const configureStore = () => {
-  const store = createStore(adminApp)
+  const store = createStore(
+    adminApp,
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
+  )
 
   if (module.hot) {
     module.hot.accept('./reducers', () => {
