4a9abfe64a40cbef08d98fd2129cf624ffa6ae5c
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-15 13:40:16 -0600

    Reducers for fetching customfields

--- app/web/reducers/customFields.js
+++ app/web/reducers/customFields.js
@@ -1,7 +1,20 @@
+import { RECEIVE_CUSTOMFIELDS, REQUEST_CUSTOMFIELDS } from '../constants'
 
+const initialState = {
+  isFetching: false,
+  items: {}
+}
 
-function customFields (state = [], action) {
+function customFields (state = initialState, action) {
   switch (action.type) {
+    case REQUEST_CUSTOMFIELDS:
+      return {...state, isFetching: true}
+    case RECEIVE_CUSTOMFIELDS: 
+      return {
+        ...state,
+        isFetching: false,
+        items: action.items.reduce((acc, product) => ({...acc, [product._id]: product }), {})
+      }
     default:
       return state
   }
