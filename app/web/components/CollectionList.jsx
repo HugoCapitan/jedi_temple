d47a3239c3120d7bf0e54c1d97a1855b1e7b6310
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-14 18:26:32 -0600

    changed edit method name

--- app/web/components/CollectionList.jsx
+++ app/web/components/CollectionList.jsx
@@ -4,7 +4,7 @@ import Avatar from 'material-ui/Avatar'
 import { List, ListItem } from 'material-ui/List'
 import IconContentModeEdit from 'material-ui/svg-icons/editor/mode-edit'
 
-const CollectionList = ({ items, onItemEdit }) => (
+const CollectionList = ({ items, onEdit }) => (
   <div>
     <List>
       {items.map((item, index) => (
@@ -15,7 +15,7 @@ const CollectionList = ({ items, onItemEdit }) => (
           secondaryTextLines={2}
           leftAvatar={<Avatar src={item.avatar} />}
           rightIcon={<IconContentModeEdit />}
-          onClick={() => onItemEdit(item)}
+          onClick={ () => onEdit(item._id) }
         />
       ))}
     </List>
