104afe039774f0098513c1e4072e3d4b961f4645
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-14 20:20:51 -0600

    Function definition for updateProduct Thunk

--- app/web/actions/network.js
+++ app/web/actions/network.js
@@ -1,15 +1,6 @@
 import axios from 'axios'
 import { receiveProducts, requestProducts } from './products'
-import { failedRequest } from './ui'
-
-export const requestCollection = collection => ({
-  type: `REQUEST_${collection.toUpperCase()}`
-})
-
-export const receiveCollection = (collection, items) => ({
-  type: `RECEIVE_${collection.toUpperCase()}`,
-  items
-})
+import { failedRequest, finishRequest, startRequest } from './ui'
 
 export const fetchCollection = (token, collection) => dispatch => {
   let endpoint = collection
@@ -25,4 +16,19 @@ export const fetchCollection = (token, collection) => dispatch => {
       response => dispatch(receiveCollection(collection, response.data)),
       error => dispatch(failedRequest(error.response.data.message))
     )
+}
+
+export const updateProduct = (newProduct) => (dispatch, getState) => {
+  const oldProduct = getState().products.items[newProduct._id]
+}
+
+export function requestCollection (collection) {
+  return { type: `REQUEST_${collection.toUpperCase()}` }
+}
+
+export function receiveCollection (collection, items) {
+  return {
+    type: `RECEIVE_${collection.toUpperCase()}`,
+    items
+  }
 }
