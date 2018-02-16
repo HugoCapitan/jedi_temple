0dcd1b2315a5f466e21d41505de53ce81a6168d1
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-16 11:34:52 -0600

    Adding list and header

--- app/web/components/ProductsConfig.jsx
+++ app/web/components/ProductsConfig.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 
+import ListHeader from './ListHeader'
+import CollectionList from './CollectionList'
+
 import dialogStyles from '../styles/dialogs'
 
 const customContentStyle = {
@@ -9,7 +12,7 @@ const customContentStyle = {
   maxWidth: 'none',
 }
 
- const ProductsConfig = ({ customs, onDone }) => {
+ const ProductsConfig = ({ customs, onCustomSelect, onCustomAdd, onDone }) => {
   const actions = [
     <FlatButton
       label="Done"
@@ -20,15 +23,17 @@ const customContentStyle = {
 
   return (
     <Dialog
-        title={'Customize Products Fields'}
         actions={actions}
-        modal={true}
+        bodyStyle={ {paddingLeft: '0', paddingRight: '0'} }
         contentStyle={customContentStyle}
+        modal={true}
         open={true}
+        title={'Customize Products Fields'}
     >
 
       <div className={dialogStyles['small-span']}>
-        hi
+        <ListHeader title="Fields" onAdd={onCustomAdd} />
+        <CollectionList items={customs} />
       </div>
 
       <div className={dialogStyles['big-span']}>
