27a04761269003d8e9da4002312e729f2b9b6bfb
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-16 13:04:24 -0600

    Custom ooptions for number field

--- app/web/components/CustomFieldEdit.jsx
+++ app/web/components/CustomFieldEdit.jsx
@@ -1,19 +1,66 @@
 import React from 'react'
 
+import CheckBox from 'material-ui/Checkbox'
 import Divider from 'material-ui/Divider'
+import FlatButton from 'material-ui/FlatButton'
 import List from 'material-ui/List/List'
 import ListItem from 'material-ui/List/ListItem'
+import RadioButton from 'material-ui/RadioButton'
+import RadioButtonGroup from 'material-ui/RadioButton/RadioButtonGroup'
 import Subheader from 'material-ui/Subheader'
-import Toggle from 'material-ui/Toggle'
+import TextField from 'material-ui/TextField'
 
-const CustomFieldEdit = ({}) => (
-  <div>
-      <List>
-        <Subheader>General</Subheader>
-        <ListItem primaryText="Type"/>
-        <ListItem primaryText="Show in site" rightToggle={<Toggle />} />
-        <ListItem primaryText="Allow filter" rightToggle={<Toggle />} />
-      </List>
+import dialogStyles from '../styles/dialogs'
+
+const CustomFieldEdit = ({ custom, onSave }) => (
+  <div class={dialogStyles['left-border']}>
+    <List>
+      <ListItem disabled={true} primaryText={`Type: ${custom.type}`}/>
+      <ListItem primaryText="Show in site" leftCheckbox={<CheckBox checked={custom.show} />} />
+      <ListItem primaryText="Allow filter" leftCheckbox={<CheckBox checked={custom.filter} />} />
+    </List>
+
+    <Divider />
+    { custom.type === 'number' ?
+    <List>
+      <Subheader>Number Field Configuration</Subheader>
+      <ListItem disabled={true} innerDivStyle={{ paddingTop: '0' }}>
+        <TextField 
+          floatingLabelText="Min Value (Leave blank for auto)"
+          fullWidth={true}
+          value={custom.min && custom.min != 'auto' ? custom.min : undefined} 
+        />
+        <TextField 
+          floatingLabelText="Max Value" 
+          fullWidth={true}
+          value={custom.max && custom.max != 'auto' ? custom.max : undefined} 
+        />
+        <TextField 
+          floatingLabelText="Unit (For display porpouses)" 
+          fullWidth={true}
+          value={custom.unit} 
+        />
+        <p>Unit Position</p>        
+        <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
+          <RadioButton
+            value="light"
+            label="Right"
+          />
+          <RadioButton
+            value="not_light"
+            label="Left"
+          />
+        </RadioButtonGroup>
+      </ListItem>
+    </List>
+    :
+    'hey'
+    }
+    <FlatButton
+      label="Save"
+      primary={true}
+      onClick={onSave}
+    />
   </div>
 )
 
