615c7afe5f5160a33c2aaa74ab3d1ff5c7901a62
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-16 11:35:44 -0600

    Sending better formatted customs

--- app/web/containers/ProductsConfigDialog.jsx
+++ app/web/containers/ProductsConfigDialog.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Avatar from 'material-ui/Avatar'
+import {
+  blue500,
+  orange500
+} from 'material-ui/styles/colors'
 
 import ProductsConfig from '../components/ProductsConfig'
 
 const mapStateToProps = state => ({
-
+  customs: getCustoms(state.customFields.items, state.ui.route)
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -17,3 +22,14 @@ const ProductsConfigDialog = connect(
 )(ProductsConfig)
 
 export default ProductsConfigDialog
+
+
+function getCustoms (items, route) {
+  return Object.values(items)
+    .filter(it => it.store === route)
+    .map(it => ({ ...it, primaryText: it.name, avatar: getAvatar(it.type) }))
+}
+
+function getAvatar(type) {
+  return type === 'string' ? <Avatar backgroundColor={orange500}>S</Avatar> : <Avatar backgroundColor={blue500}>N</Avatar>
+}
