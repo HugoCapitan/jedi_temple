39410f1445f6bd64be6191a80f44e79d80da67d2
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-15 21:36:49 -0600

    Full updateProductRequest action implementation

--- app/web/actions/network.js
+++ app/web/actions/network.js
@@ -1,8 +1,8 @@
 import _     from 'lodash'
 import axios from 'axios'
 
-import { receiveProducts, requestProducts, updateProduct } from './products'
-import { failedRequest, finishRequest, startRequest } from './ui'
+import { addProduct, receiveProducts, requestProducts, updateProduct } from './products'
+import { failedRequest, finishRequest, startRequest, closeItemDialog } from './ui'
 
 export const fetchCollection = (token, collection) => dispatch => {
   let endpoint = collection
@@ -20,8 +20,27 @@ export const fetchCollection = (token, collection) => dispatch => {
     )
 }
 
+export const requestAddProduct = newProduct => (dispatch, getState) => {
+  const token = getState().authToken
+  console.log(newProduct)
+
+  dispatch(startRequest())
+  return axios.post('/api/products/', newProduct, {
+    headers: { 'Authorization': 'Bearer ' + token }
+  })
+  .then(
+    response => {
+      dispatch(addProduct(response.data))
+      dispatch(finishRequest('Product Saved'))
+      dispatch(closeItemDialog())
+    },
+    error => dispatch(failedRequest('Error adding product'))
+  )
+}
+
 export const requestProductUpdate = (oldProduct, newProduct) => (dispatch, getState) => {
   const token = getState().authToken
+  const reqOptions = { headers: { 'Authorization': 'Bearer ' + token } }
   const customsReqs = []
   const removedCustoms = _.differenceWith(oldProduct.customs, newProduct.customs, (oldVal, newVal) => 
     oldVal.custom_id == newVal.custom_id
@@ -32,45 +51,36 @@ export const requestProductUpdate = (oldProduct, newProduct) => (dispatch, getSt
   const modifiedCustoms = newProduct.customs.filter(newCustom => 
     oldProduct.customs.find(oldCustom => newCustom.custom_id == oldCustom.custom_id && newCustom.value != oldCustom.value)
   )
-  const productToSend = { name: newProduct.name, price: newProduct.pricem, stock: newProduct.stock, description: newProduct.description }
+  const productToSend = { name: newProduct.name, price: newProduct.price, stock: newProduct.stock, description: newProduct.description }
   
   dispatch(startRequest())
-  
-  customsReqs.push(axios.put(`/api/products/${oldProduct._id}/`, productToSend, {
-    headers: { 'Authorization': 'Bearer ' + token }
-  }))
+
+  customsReqs.push( axios.put(`/api/products/${oldProduct._id}/`, productToSend, reqOptions) )
   
   for (const removed of removedCustoms) {
-    customsReqs.push(
-      axios.delete(`/api/products/${oldProduct._id}/customs/${removed._id}/`, {
-        headers: { 'Authorization': 'Bearer ' +  token }
-      })
-    )
+    customsReqs.push( axios.delete(`/api/products/${oldProduct._id}/customs/${removed._id}/`, reqOptions) )
   }
 
   for (const added of addedCustoms) {
-    customsReqs.push(
-      axios.post(`/api/products/${oldProduct._id}/customs/`, added, {
-        headers: { 'Authorization': 'Bearer ' + token }
-      })
-    )
+    customsReqs.push( axios.post(`/api/products/${oldProduct._id}/customs/`, added, reqOptions) )
   }
 
   for (const modified of modifiedCustoms) {
-    customsReqs.push(
-      axios.put(`/api/products/${oldProduct._id}/customs/${modified._id}/`, modified, {
-        headers: { 'Authorization': 'Bearer ' + token }
-      })
-    )
+    customsReqs.push( axios.put(`/api/products/${oldProduct._id}/customs/${modified._id}/`, modified, reqOptions) )
   }
 
   return Promise.all(customsReqs)
     .then(
-      response => {
-        dispatch(updateProduct(newProduct))
-        dispatch(finishRequest('Product Updated'))
-      },
+      responses => axios.get(`/api/products/${oldProduct._id}/`, reqOptions),
       error => dispatch(failedRequest('Product Update Failed'))
+    )
+    .then(
+      response => {
+        dispatch(updateProduct(response.data))
+        dispatch(finishRequest('Product Updated'))
+        dispatch(closeItemDialog())
+      },
+      error => dispatch(failedRequest('Please Reload the window'))
     )
 }
 
