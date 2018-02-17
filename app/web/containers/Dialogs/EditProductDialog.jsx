8824e57296875ee6cfce4abd6e0f8936ec496364
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-17 09:58:51 -0600

    Dialog for product edition form

---
+++ app/web/containers/Dialogs/EditProductDialog.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Dialog      from 'material-ui/Dialog'
+import FlatButton  from 'material-ui/FlatButton'
+
+import { closeItemDialog, requestProductUpdate, requestAddProduct } from '../actions'
+
+import EditProductForm from '../../components/forms/EditProductForm'
+
+const component = ({ open, title, product, customs, onSave, onCancel }) => {
+  const actions = [
+    <FlatButton
+      label="Cancel"
+      primary={true}
+      onClick={onCancel}
+    />
+  ]
+
+  return (
+    <Dialog
+      title={title}
+      actions={actions}
+      modal={true}
+      contentStyle={{ width: '100%', maxWidth: 'none' }}
+      open={this.props.open}
+    >
+      <EditProductForm 
+        product={product}
+        customs={customs}
+        onSave={onSave}
+      />
+    </Dialog>
+  )
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  open: state.ui.itemDialog.open,
+  title: state.ui.itemDialog.itemID ? 'Edit Product' : 'New Product',
+  product: getProduct(state.products.items, state.ui.itemDialog.itemID, state.ui.route),
+  customs: Object.values(state.customFields.items).filter(custom => custom.store === state.ui.route)
+})
+
+const mapDispatchToProps = dispatch => ({
+  onSave(oldProduct, newProduct) { state.ui.itemDialog.itemID 
+    ? dispatch(requestProductUpdate(oldProduct, newProduct)) 
+    : dispatch(requestAddProduct(newProduct)) 
+  },
+  onCancel() { dispatch(closeItemDialog()) }
+})
+
+const ProductEditDialog = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductEdit)
+
+export default ProductEditDialog
+
+
+function getProduct(products, itemID, route) {
+  const foundProduct = !!products[itemID] 
+    ? products[itemID]
+    : { customs: [], description: '', images: [], name: '', price: undefined, stock: undefined, store: route }
+  return { ...foundProduct }
+}
