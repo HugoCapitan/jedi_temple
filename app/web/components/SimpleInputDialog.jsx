b9be8a976fef58c703779741301eee35ba3b949e
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-19 15:22:52 -0600

    New component for simple inputs

---
+++ app/web/components/SimpleInputDialog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Dialog from 'material-ui/Dialog'
+import TextField from 'material-ui/TextField'
+
+const SimpleInputDialog = ({ title, value, onDone, onCancel }) => (
+  <Dialog>
+    
+  </Dialog>
+)
