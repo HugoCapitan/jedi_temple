195e0df5fb78cbf207ebe7e13255df271f6d2ae2
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-19 11:53:07 -0600

    fixes and refactoring

--- app/web/components/forms/CustomNumberEdit.jsx
+++ app/web/components/forms/CustomNumberEdit.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 
-import CheckBox from 'material-ui/Checkbox'
-import Divider from 'material-ui/Divider'
-import FlatButton from 'material-ui/FlatButton'
-import IconButton from 'material-ui/IconButton'
-import IconContentAddCircle from 'material-ui/svg-icons/content/add-circle-outline'
-import IconContentRemoveCircle from 'material-ui/svg-icons/content/remove-circle-outline'
-import List from 'material-ui/List/List'
-import ListItem from 'material-ui/List/ListItem'
-import RadioButton from 'material-ui/RadioButton'
+import CheckBox         from 'material-ui/Checkbox'
+import Divider          from 'material-ui/Divider'
+import FlatButton       from 'material-ui/FlatButton'
+import List             from 'material-ui/List/List'
+import ListItem         from 'material-ui/List/ListItem'
+import RadioButton      from 'material-ui/RadioButton'
 import RadioButtonGroup from 'material-ui/RadioButton/RadioButtonGroup'
-import Subheader from 'material-ui/Subheader'
-import TextField from 'material-ui/TextField'
+import TextField        from 'material-ui/TextField'
 
-import dialogStyles from '../styles/dialogs'
+import dialogStyles from '../../styles/dialogs'
 
-const CustomFieldEdit = ({ custom, onSave }) => (
+const CustomFieldEdit = ({ custom, formActions }) => (
   <div className={dialogStyles['left-border']}>
     <List>
       <ListItem disabled={true} primaryText={`Type: ${custom.type}`}/>
       <ListItem primaryText="Show in site" leftCheckbox={<CheckBox checked={custom.show} />} />
       <ListItem primaryText="Allow filter" leftCheckbox={<CheckBox checked={custom.filter} />} />
       <Divider />
-    { custom.type === 'number' ?
       <ListItem disabled={true} innerDivStyle={{ paddingTop: '0' }}>
         <TextField 
           floatingLabelText="Min Value (Leave blank for auto)"
@@ -51,48 +46,16 @@ const CustomFieldEdit = ({ custom, onSave }) => (
           />
         </RadioButtonGroup>
       </ListItem>
-    :
-      <div>
-        <ListItem 
-          disabled={true}
-          primaryText="Values"
-          initiallyOpen={true}
-          rightIconButton={(
-            <IconButton>
-              <IconContentAddCircle />
-            </IconButton>
-          )}
-        />
-        <List>
-          <Divider inset={true}/>
-          {custom.values.map(cValue => (
-            <ListItem 
-              primaryText={cValue.value}
-              insetChildren={true}
-              rightIconButton={
-                <IconButton onClick={}>
-                  <IconContentRemoveCircle />
-                </IconButton>
-              }
-          />
-          ))}
-        </List>
-      </div>
-    }
     </List>
-    <div style={{ display: 'inline-block', width:'100%' }}>
-      <FlatButton
-        label="Save"
-        onClick={onSave}
-        primary={true}
-        style={{ float: 'right',  margin: '0 10px 5px 0' }}
-      />
-      <FlatButton
-        label="Cancel"
-        onClick={onSave}
-        primary={true}
-        style={{ float: 'right',  margin: '0 0 5px 0' }}
-      />
+    <div className={dialogStyles['bottom-buttons']}>
+      {formActions.map((action, index) => 
+        <FlatButton
+          label={action.label}
+          onClick={action.onClick}
+          primary={true}
+          style={{ float: 'right',  margin: '0 10px 5px 0' }}
+        />
+      )}
     </div>
   </div>
 )
