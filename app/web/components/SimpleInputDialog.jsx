0a32267dbdff4458ee20ba7fc7ba259b087f69ba
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-20 09:33:51 -0600

    Finished SimpleInputDialog

--- app/web/components/SimpleInputDialog.jsx
+++ app/web/components/SimpleInputDialog.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 
-import Dialog from 'material-ui/Dialog'
-import TextField from 'material-ui/TextField'
+import Dialog     from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import TextField  from 'material-ui/TextField'
 
-const SimpleInputDialog = ({ title, value, onDone, onCancel }) => (
-  <Dialog>
-    
-  </Dialog>
-)
+const SimpleInputDialog = ({ open, title, value, onDone, onCancel }) => {
+  const customStyles = {
+    width: 'auto',
+    display: 'inline-block',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+
+  const actions = [
+    <FlatButton
+      label="Cancel"
+      onClick={onCancel}
+    />,
+    <FlatButton
+      label="Done"
+      onClick={onDone}
+      primary={true}   
+    />
+  ]
+  
+  return (
+    <Dialog
+      actions={actions}
+      open={open}
+      title={title}
+      contentStyle={customStyles}
+    >
+      <TextField 
+      />
+    </Dialog>
+  )
+}
+
+export default SimpleInputDialog
