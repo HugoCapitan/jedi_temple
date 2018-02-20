cf917831f371f5c1225809feca05f3db6420dee2
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-20 13:45:02 -0600

    Closing unnecessary routes

--- app/api/routes/client.js
+++ app/api/routes/client.js
@@ -8,18 +8,4 @@ module.exports = app => {
     .get(clientCtrl.apiRead)
     .put(clientCtrl.apiUpdate)
     .delete(clientCtrl.apiRemove)
-
-  app.post('/clients/:client_id/addresses/', clientCtrl.apiCreateAddress)
-  app.route('/clients/:client_id/addresses/:address_id')
-    .put(clientCtrl.apiUpdateAddress)
-    .delete(clientCtrl.apiRemoveAddress)
-
-  app.post('/clients/:client_id/orders/:order_id', clientCtrl.apiAddOrder)
-  app.delete('/clients/:client_id/orders/:order_id', clientCtrl.apiRemoveOrder)
-
-  app.post('/clients/:client_id/reservations/:reservation_id', clientCtrl.apiAddReservation)
-  app.delete('/clients/:client_id/reservations/:reservation_id', clientCtrl.apiRemoveReservation)
-
-  app.post('/clients/:client_id/wishlist/:wish_id', clientCtrl.apiAddWish)
-  app.delete('/clients/:client_id/wishlist/:wish_id', clientCtrl.apiRemoveWish)
 }
