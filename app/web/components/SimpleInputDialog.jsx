629c414b9575eee8ba9b99f2da3e1d361452e844
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-20 09:42:05 -0600

    Moving to class component for handling input

--- app/web/components/SimpleInputDialog.jsx
+++ app/web/components/SimpleInputDialog.jsx
@@ -1,42 +1,70 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Dialog     from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import TextField  from 'material-ui/TextField'
 
-const SimpleInputDialog = ({ open, title, value, onDone, onCancel }) => {
-  const customStyles = {
-    width: 'auto',
-    display: 'inline-block',
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)'
+class SimpleInputDialog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: '' }
   }
 
-  const actions = [
-    <FlatButton
-      label="Cancel"
-      onClick={onCancel}
-    />,
-    <FlatButton
-      label="Done"
-      onClick={onDone}
-      primary={true}   
-    />
-  ]
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleDone() {
+    this.props.onDone(this.state.text)
+  }
+
+  render() {
+    const customStyles = {
+      width: 'auto',
+      display: 'inline-block',
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }
   
-  return (
-    <Dialog
-      actions={actions}
-      open={open}
-      title={title}
-      contentStyle={customStyles}
-    >
-      <TextField 
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        onClick={this.props.onCancel}
+      />,
+      <FlatButton
+        label="Done"
+        onClick={this.handleDone.bind(this)}
+        primary={true}   
       />
-    </Dialog>
-  )
+    ]
+    
+    return (
+      <Dialog
+        actions={actions}
+        open={this.props.open}
+        title={this.props.title}
+        contentStyle={customStyles}
+      >
+        <TextField 
+          value={this.state.inputValue}
+          onChange={this.handleChange.bind(this)}
+        />
+      </Dialog>
+    )
+  }
 }
 
 export default SimpleInputDialog
+
+
+SimpleInputDialog.propTypes = { 
+  open: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  onDone: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired
+}
