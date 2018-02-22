cd70a0c6fe16a0f43b364db68da5e9eca2474dfb
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-22 12:23:30 -0600

    super cleaner product update action

--- app/web/actions/network.js
+++ app/web/actions/network.js
@@ -125,50 +125,20 @@ export function requestProductRemove(productID) {
   }
 }
 
-export function requestProductUpdate(newProduct, oldProduct) {
+export function requestProductUpdate(newProduct) {
   return (dispatch, getState) => {
-    const token = getState().authToken
-    const reqOptions = { headers: { 'Authorization': 'Bearer ' + token } }
-    const requests = []
-    const removedCustoms = _.differenceWith(oldProduct.customs, newProduct.customs, (oldVal, newVal) => 
-      oldVal.custom_id == newVal.custom_id
-    )
-    const addedCustoms = _.differenceWith(newProduct.customs, oldProduct.customs, (newVal, oldVal) => 
-      oldVal.custom_id == newVal.custom_id
-    )
-    const modifiedCustoms = newProduct.customs.filter(newCustom => 
-      oldProduct.customs.find(oldCustom => newCustom.custom_id == oldCustom.custom_id && newCustom.value != oldCustom.value)
-    )
-    const productToSend = { name: newProduct.name, price: newProduct.price, stock: newProduct.stock, description: newProduct.description }
-    
-    dispatch(startRequest())
-  
-    requests.push( axios.put(`/api/products/${oldProduct._id}/`, productToSend, reqOptions) )
-    
-    for (const removed of removedCustoms) {
-      requests.push( axios.delete(`/api/products/${oldProduct._id}/customs/${removed._id}/`, reqOptions) )
-    }
-  
-    for (const added of addedCustoms) {
-      requests.push( axios.post(`/api/products/${oldProduct._id}/customs/`, added, reqOptions) )
-    }
-  
-    for (const modified of modifiedCustoms) {
-      requests.push( axios.put(`/api/products/${oldProduct._id}/customs/${modified._id}/`, modified, reqOptions) )
-    }
-  
-    return Promise.all(requests)
-      .then(
-        responses => axios.get(`/api/products/${oldProduct._id}/`, reqOptions),
-        error => dispatch(failedRequest('Product Update Failed'))
-      )
+    const token     = getState().authToken
+    const productID = newProduct._id
+    const update    = formatProductToSend(newProduct)
+
+    return axios.put(`/api/products/${productID}/`, update, { headers: { 'Authorization': 'Bearer ' + token } })
       .then(
         response => {
           dispatch(updateProduct(response.data))
           dispatch(finishRequest('Product Updated'))
           dispatch(closeItemDialog())
         },
-        error => dispatch(failedRequest('Please Reload the window'))
+        error => dispatch(failedRequest('Product Update Failed'))
       )
   }
 }
@@ -183,4 +153,15 @@ export function receiveCollection (collection, items) {
     type: `RECEIVE_${collection.toUpperCase()}`,
     items
   }
+}
+
+
+function formatProductToSend(product) { 
+  delete product.created_at
+  delete product.slug
+  delete product.store
+  delete product.updated_at
+  delete product.__v
+
+  return product
 }
