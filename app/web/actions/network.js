8c2924bf8c73c6ce9883cd9c6adeff5b2f856fb9
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-22 12:33:17 -0600

    much much cleaner custom update function

--- app/web/actions/network.js
+++ app/web/actions/network.js
@@ -1,7 +1,7 @@
 import _     from 'lodash'
 import axios from 'axios'
 
-import { addCustom } from './customFields'
+import { addCustom, updateCustom, removeCustom } from './customFields'
 import { addProduct, removeProduct, updateProduct } from './products'
 import { failedRequest, finishRequest, startRequest, closeItemDialog, closeSettingsDialog } from './ui'
 
@@ -49,42 +49,22 @@ export function requestCustomRemove(){
   }
 }
 
-export function requestCustomUpdate (newCustom, oldCustom) {
+export function requestCustomUpdate (newCustom) {
   return (dispatch, getState) => {
-    const token = getState().authToken
-    const reqOptions = { headers: { 'Authorization': 'Bearer ' + token } }
-    const requests = []
+    const token    = getState().authToken
+    const customID = newCustom._id
+    const update   = formatObjectToSend(newCustom)
+    delete update.type
+    delete update._values
   
-    let customToSend = { name: newCustom.name, show: newCustom.show, filter: newCustom.filter }
-  
-    if (oldCustom.type === 'string') {
-      const newValues = _.differenceWith(newCustom.values, oldCustom.values, (newVal, oldVal) => oldVal.value === newVal.value)
-      const removedValues = _.differenceWith(oldCustom.values, newCustom.values, (oldVal, newVal) => oldVal.value === newVal.value)
-  
-      requests.push(
-        ...newValues.map(newVal => axios.post(`/api/custom_fields/${oldCustom._id}/values`, newVal, reqOptions)),
-        ...removedValues.map(remVal => axios.delete(`/api/custom_fields/${oldCustom._id}/values/${remVal._id}/`, reqOptions))
-      )
-    } else if (oldCustom.type === 'number') {
-      customToSend = { ...customToSend, 
-        unit: newCustom.unit, unit_place: newCustom.unit_place, min: newCustom.min, max: newCustom.max 
-      }
-    }
-  
-    requests.push( axios.put(`/api/custom_fields/${oldCustom._id}/`, customToSend, reqOptions) )
-  
-    return Promise.all(requests)
-      .then(
-        responses => axios.get(`/api/custom_fields/${oldCustom._id}/`, reqOptions),
-        error => dispatch(failedRequest('Product Update Failed'))
-      )
+    return axios.put(`/api/custom_fields/${customID}/`, update, { headers: { 'Authorization': 'Bearer ' + token } })
       .then(
         response => {
           dispatch(updateCustom(response.data))
           dispatch(finishRequest('Custom Updated'))
           dispatch(closeSettingsDialog())
         },
-        error => dispatch(failedRequest('Please Reload the window'))
+        error => dispatch(failedRequest('Failed CustomField update request'))
       )
   }
 }
@@ -129,7 +109,7 @@ export function requestProductUpdate(newProduct) {
   return (dispatch, getState) => {
     const token     = getState().authToken
     const productID = newProduct._id
-    const update    = formatProductToSend(newProduct)
+    const update    = formatObjectToSend(newProduct)
 
     dispatch(startRequest())
     return axios.put(`/api/products/${productID}/`, update, { headers: { 'Authorization': 'Bearer ' + token } })
@@ -156,13 +136,12 @@ export function receiveCollection (collection, items) {
   }
 }
 
+function formatObjectToSend(toSend) { 
+  delete toSend.created_at
+  delete toSend.slug
+  delete toSend.store
+  delete toSend.updated_at
+  delete toSend.__v
 
-function formatProductToSend(product) { 
-  delete product.created_at
-  delete product.slug
-  delete product.store
-  delete product.updated_at
-  delete product.__v
-
-  return product
+  return toSend
 }
