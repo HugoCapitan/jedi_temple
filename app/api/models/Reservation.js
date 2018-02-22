254329118fa25fe54fc9f5adc41e6861e4b41e3e
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-22 12:01:32 -0600

    Reservation middleware correct validation and modification of total in preUpdate middleware

--- app/api/models/Reservation.js
+++ app/api/models/Reservation.js
@@ -47,34 +47,42 @@ ReservationSchema._middlewareFuncs = {
   preSave(next) {
     const self = this
 
+    if (!self.isNew && self.isModified('store')) {
+      const err = new Error('Store is not updatable')
+      err.name = 'ValidationError'
+      return next(err)
+    }
+
     const currentDate = new Date()
     self.updated_at = currentDate
     if (!self.created_at) self.created_at = currentDate
 
-    if (!self.hasOwnProperty('total')) self.total = moment(self.departure_date).diff(self.arrive_date, 'days') * self.night_price
+    self.total = moment(self.departure_date).diff(self.arrive_date, 'days') * self.night_price
 
     return next()
   },
   preUpdate(next) {
     const self = this
 
-    self._update.updated_at = new Date()
+    if (self._update.hasOwnProperty('store')) {
+      const err = new Error('Store is not updatable')
+      err.name = 'ValidationError'
+      return next(err)
+    }
 
-    if (self._update.hasOwnProperty('arrive_date')) {
-      const err = new Error('arrive_date should be updated via Save')
+    if ((self._update.hasOwnProperty('arrive_date') || self._update.hasOwnProperty('departure_date') || 
+         self._update.hasOwnProperty('night_price')) && (!self._update.hasOwnProperty('arrive_date') ||
+        !self._update.hasOwnProperty('departure_date') || !self._update._hasOwnProperty('night_price'))
+    ) {
+      const err = new Error('arrive_date, departure_date and night_price should be together in case of update of any')
       err.name = 'ValidationError'
       return next(err)
     }
-    if (self._update.hasOwnProperty('departure_date')) {
-      const err = new Error('departure_date should be updated via Save')
-      err.name = 'ValidationError'
-      return next(err)
-    }
-    if  (self._update.hasOwnProperty('night_price')) {
-      const err = new Error('night_price should be updated via Save')
-      err.name = 'ValidationError'
-      return next(err)
-    }
+
+    if (self._update.hasOwnProperty('arrive_date'))
+      self._update.total = moment(self._update.departure_date).diff(self._update.arrive_date, 'days') * self._update.night_price
+
+    self._update.updated_at = new Date()
 
     return next()
   }
