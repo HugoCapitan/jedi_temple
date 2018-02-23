3c750bcce7a6235c69fb7891542831b4b0e0719c
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-22 18:46:19 -0600

    importing and using customsSection in store

--- app/web/containers/Store.jsx
+++ app/web/containers/Store.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import GeneralSection from './GeneralSection'
+import CustomsSection from './CustomsSection'
 
 const EStoreComponent = ({ section }) => {
   let displaySection = ''
 
   if (section === 'general') displaySection = <GeneralSection />
+  if (section === 'customs') displaySection = <CustomsSection />
 
   return (
     <div>
