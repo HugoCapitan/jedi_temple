f45a9c01ed8c0479900a41dfa60a353155e22847
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-02-25 21:19:27 -0600

    first reducers for fetching orders

--- app/web/reducers/orders.js
+++ app/web/reducers/orders.js
@@ -1,7 +1,22 @@
+import { REQUEST_ORDERS, RECEIVE_ORDERS } from '../constants'
 
+const initialState = {
+  isFetching: false,
+  items: []
+}
 
-function orders (state = [], action) {
+function orders (state = initialState, action) {
   switch (action.type) {
+    case RECEIVE_ORDERS:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case REQUEST_ORDERS:
+      return {
+        ...state,
+        items: action.orders.reduce((acc, order) => ({...acc, [order._id]: order }), {})
+      }
     default:
       return state
   }
