d1af253f333426ac0878b6bb776f3351a0f5b8cd
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-03 19:06:23 -0600

    First app component

---
+++ app/web/components/App.jsx
@@ -0,0 +1,33 @@
+import _ from 'lodash'
+import React from 'react'
+import { connect } from 'react-redux'
+
+import AppBar from 'material-ui/AppBar'
+import SnackBar from 'material-ui/Snackbar'
+
+import { toggleDrawer } from '../actions'
+
+const Component = ({ store, error, toggleDrawer }) => (
+  <div>
+    <AppBar 
+      title={store != 'general' ? _.capitalize(store) : 'Heberto Sites Admin' }
+      onLeftIconButtonClick={toggleDrawer}
+    />
+  </div>
+)
+
+const mapStateToProps = state => ({
+  store: state.ui.route,
+  error: state.ui.requestError
+})
+
+const mapDispatchToProps = dispatch => ({
+  toggleDrawer() { dispatch(toggleDrawer()) }
+})
+
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Component)
+
+export default App
