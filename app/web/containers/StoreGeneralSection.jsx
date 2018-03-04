c2df0473be45794c63c35f41b743df1d2851f85f
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-03 19:45:55 -0600

    First implementation of StoreGeneralSection component

--- app/web/containers/StoreGeneralSection.jsx
+++ app/web/containers/StoreGeneralSection.jsx
@@ -1,2 +1,60 @@
 import React from 'react'
-import { conect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+// import ProductsTile from '../components/ProductsTile'
+import Tile from '../components/Tile'
+
+const StoreGeneralSectionComponent = ({ messages, messagesActions, orders, ordersActions, products, productsActions, tops, topsActions }) => (
+  <div>
+    <Tile 
+      title="Orders"
+      items={orders}
+      actions={ordersActions}
+    />
+  </div>
+)
+
+StoreGeneralSectionComponent.propTypes = {
+  messages: PropTypes.array.isRequired,
+  messagesActions: PropTypes.object.isRequired,
+  orders: PropTypes.array.isRequired,
+  ordersActions: PropTypes.object.isRequired,
+  products: PropTypes.array.isRequired,
+  productsActions: PropTypes.object.isRequired,
+  tops: PropTypes.array.isRequired,
+  topsActions: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  messages: [],
+  orders: [],
+  products: [],
+  tops: []
+})
+
+const mapDispatchToProps = dispatch => ({
+  messagesActions: {
+    select: ''
+  },
+  ordersActions: {
+    select: ''
+  },
+  productsActions: {
+    add: '',
+    remove: '',
+    select: ''
+  },
+  topsActions: {
+    add: '',
+    remove: '',
+    select: ''
+  }
+})
+
+const StoreGeneralSection = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StoreGeneralSectionComponent)
+
+export default StoreGeneralSection
