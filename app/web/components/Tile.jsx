8fb3682671f450682aff350f6ab6db16de5e8aef
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-05 10:05:46 -0600

    Using TileHeader Component

--- app/web/components/Tile.jsx
+++ app/web/components/Tile.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import TileHeader from './TileHeader'
+
 const Tile = ({ actions, items, size, title }) => (
   <div>
-    
+    <TileHeader 
+      title={title}
+      addAction={actions.add}
+    /> 
   </div>
 )
 
 Tile.propTypes = {
-  actions: PropTypes.object.isRequired,
+  actions: PropTypes.shape({
+    add: PropTypes.func,
+    select: PropTypes.select,
+    remove: PropTypes.func
+  }).isRequired,
   items: PropTypes.array.isRequired,
   size: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
