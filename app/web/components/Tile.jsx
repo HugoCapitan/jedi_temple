109c98a97eceb00a84feabc8d138ae42938beddf
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-05 10:27:50 -0600

    Implementing list component and new header props

--- app/web/components/Tile.jsx
+++ app/web/components/Tile.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Paper from 'material-ui/Paper'
+
+import CollectionList from './CollectionList'
 import TileHeader from './TileHeader'
 
-const Tile = ({ actions, items, size, title }) => (
-  <div>
+const Tile = ({ actions, goBack, items, size, title }) => (
+  <Paper>
     <TileHeader 
       title={title}
       addAction={actions.add}
-    /> 
-  </div>
+      backAction={goBack}
+      configAction={actions.config}
+    />
+    <CollectionList 
+      items={items}
+      onRemove={actions.remove}
+      onSelect={actions.select}
+    />
+  </Paper>
 )
 
 Tile.propTypes = {
   actions: PropTypes.shape({
     add: PropTypes.func,
-    select: PropTypes.select,
+    config: PropTypes.func,
+    select: PropTypes.func,
     remove: PropTypes.func
   }).isRequired,
   items: PropTypes.array.isRequired,
   size: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  goBack: PropTypes.func  
 }
 
 export default Tile
