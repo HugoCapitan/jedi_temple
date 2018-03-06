99409409d915c3fceef0ec48e154dbeeee5897f2
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-06 17:56:21 -0600

    implemented all the form fields in number customfd form

--- app/web/components/CustomFdNumberForm.jsx
+++ app/web/components/CustomFdNumberForm.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Divider from 'material-ui/Divider'
 import TextField from 'material-ui/TextField'
 
 import gridStyles from '../styles/grid'
 
-const CustomFdNumberForm = ({ min, max, unit, unit_place, reportChanges }) => (
+const CustomFdNumberForm = ({ min, max, unit, unit_place, reportChange }) => (
   <div className={gridStyles['container']}>
-    
+    <TextField 
+      floatingLabelText="Min Value"
+      fullWidth={true}
+      name="min"
+      value={min}
+      onChange={ev => { reportChange('min', ev.target.value) }}
+    />
+    <TextField 
+      floatingLabelText="Max Value"
+      fullWidth={true}
+      name="max"
+      value={max}
+      onChange={ev => { reportChange('max', ev.target.value) }}
+    />
+    <TextField 
+      floatingLabelText="Unit (For display porpouses)"
+      fullWidth={true}
+      name="unit"
+      value={unit}
+      onChange={ev => { reportChange('unit', ev.target.value) }}
+    />
+    <RadioButtonGroup 
+      name="unit_place" 
+      defaultSelected="after"
+      valueSelected={unit_place}
+      onChange={(e, v) => { reportChange('unit_place', v) }}
+    >
+      <RadioButton
+        value="after"
+        label="After Value"
+      />
+      <RadioButton
+        value="before"
+        label="Before Value"
+      />
+    </RadioButtonGroup>
   </div>
 )
 
@@ -17,7 +51,7 @@ CustomFdNumberForm.propTypes = {
   max: PropTypes.number.isRequired,
   unit: PropTypes.string.isRequired,
   unit_place: PropTypes.string.isRequired,
-  reportChanges: PropTypes.func.isRequired
+  reportChange: PropTypes.func.isRequired
 }
 
 export default CustomFdNumberForm
