159393a18976744ee06d90f067592b504751e8c3
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-07 15:24:41 -0600

    ProductForm first fields implementation

--- app/web/components/ProductForm.jsx
+++ app/web/components/ProductForm.jsx
@@ -1,15 +1,60 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import TextField from 'material-ui/TextField'
+
 class ProductForm extends React.Component {
   constructor(props) {
     super(props)
     this.sate = { ...props.product }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        <div>
+          <TextField
+            floatingLabelText="Name"
+            fullWidth={true}
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          <TextField
+            floatingLabelText="Price (US$)"
+            fullWidth={true}
+            name="price"
+            onChange={this.handleChange}
+            type="number"
+            value={this.state.price}
+          />
+          <TextField
+            floatingLabelText="Stock"
+            fullWidth={true}
+            name="stock"
+            onChange={this.handleChange}
+            type="number"
+            value={this.state.stock}
+          />
+          <TextField
+            floatingLabelText="Description"
+            fullWidth={true}
+            multiLine={true}
+            name="description"
+            onChange={this.handleChange}
+            rows={5}
+            value={this.state.description}
+          />
+        </div>
+      </div>
     )
   }
 }
