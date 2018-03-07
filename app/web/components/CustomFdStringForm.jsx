5f29959d8e14cb7e130670732c6b732315eeb44f
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-06 18:19:52 -0600

    Writing customfd string form fields and names of methods

--- app/web/components/CustomFdStringForm.jsx
+++ app/web/components/CustomFdStringForm.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Divider from 'material-ui/Divider'
+import IconButton from 'material-ui/IconButton'
+import IconContentAddCircle from 'material-ui/svg-icons/content/add-circle-outline'
+import IconContentRemoveCircle from 'material-ui/svg-icons/content/remove-circle-outline'
+import ListItem from 'material-ui/List/ListItem'
+import TextField from 'material-ui/TextField'
+
+
 class CustomFdStringForm extends React.Component {
   constructor(props) {
     super(props)
@@ -10,7 +18,34 @@ class CustomFdStringForm extends React.Component {
   render() {
     return (
       <div>
-        
+        <ListItem
+          primaryText="values"
+          rightIconButton={
+            <IconButton onClick={() => { this.openInput() } }>
+              <IconContentAddCircle />
+            </IconButton>
+          }
+        />
+        <Divider inset={true} />
+        { openInput ? 
+          <TextField 
+            name={'newvalue'}
+            value={this.state.inputVal}
+            onChange={this.handleChange}
+          />
+          : ''
+        }
+        {this.props.values.map((value, index) => 
+          <ListItem
+            key={index}
+            primaryText={value.value}
+            rightIconButton={
+              <IconButton onClick={() => { this.handleRemoveValue.call(this, value.value) } }>
+                <IconContentRemoveCircle />
+              </IconButton>
+            }
+          />
+        )}
       </div>
     )
   }
