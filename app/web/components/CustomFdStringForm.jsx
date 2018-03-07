e63f7439a2e7976149e158687b3d5ff30198682b
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-07 13:56:52 -0600

    implementing methods  to add a new value in string customfd

--- app/web/components/CustomFdStringForm.jsx
+++ app/web/components/CustomFdStringForm.jsx
@@ -1,52 +1,93 @@
+import _ from 'lodash'
 import React from 'react'
 import PropTypes from 'prop-types'
 
 import Divider from 'material-ui/Divider'
 import IconButton from 'material-ui/IconButton'
+import IconActionOk from 'material-ui/svg-icons/action/done'
 import IconContentAddCircle from 'material-ui/svg-icons/content/add-circle-outline'
 import IconContentRemoveCircle from 'material-ui/svg-icons/content/remove-circle-outline'
+import IconNavigationClose from 'material-ui/svg-icons/navigation/close'
 import ListItem from 'material-ui/List/ListItem'
 import TextField from 'material-ui/TextField'
 
+import formStyles from '../styles/form'
 
 class CustomFdStringForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = { openInput: false, inputVal: '' }
+
+    this.addValue = this.addValue.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleInputKeyPress = this.handleInputKeyPress.bind(this)
+    this.removeValue = this.handleChange.bind(this)
+    this.toggleInput = this.toggleInput.bind(this)
   }
 
-  openInput() {
+  addValue() {
+    const exists = this.props.values.find(cVal => cVal.value === this.state.inputVal)
 
+    if (!exists) {
+      const newValues = [
+        { value: this.state.inputVal },
+        ...this.props.values
+      ]
+      this.props.reportChange('values', newValues)
+      this.setState({
+        openInput: false,
+        inputVal: ''
+      })
+    }
   }
 
-  handleChange() {
-
+  handleChange(ev) {
+    this.setState({
+      inputVal: ev.target.value
+    })
   }
 
-  addValue() {}
+  handleInputKeyPress(ev) {
+    if(ev.key == 'Enter') this.addValue()
+  }
 
-  removeValue() {}
+  removeValue() {}  
+
+  toggleInput() {
+    this.setState({
+      openInput: !this.state.openInput
+    })
+  }
 
   render() {
     return (
       <div>
         <p>Values:</p>
         <Divider inset={true} />
-        <ListItem
-          primaryText="Add New Value"
-          rightIconButton={
-            <IconButton onClick={() => { this.openInput() } }>
-              <IconContentAddCircle />
+        { this.state.openInput ?
+          <div className={formStyles['inline-input__container']}>
+            <TextField
+              floatingLabelText="New Value"
+              className={formStyles['inline-input__input']}
+              name={'newvalue'}
+              autoFocus={true}
+              value={this.state.inputVal}
+              onChange={this.handleChange}
+              onKeyPress={this.handleInputKeyPress}
+            />
+            <IconButton onClick={this.addValue}>
+              <IconActionOk className={formStyles['primary-action__color']} />
             </IconButton>
-          }
-        />
-        { this.state.openInput ? 
-          <TextField 
-            name={'newvalue'}
-            value={this.state.inputVal}
-            onChange={this.handleChange}
+            <IconButton onClick={this.toggleInput}>
+              <IconNavigationClose />
+            </IconButton>
+          </div>
+          :
+          <ListItem
+            primaryText="Add New Value"
+            onClick={this.toggleInput}
+            rightIcon={ <IconContentAddCircle /> }
           />
-          : ''
         }
         {this.props.values.map((value, index) => 
           <ListItem
