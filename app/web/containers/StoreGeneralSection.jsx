9d3ef219f6d26517e164f8986090c2818f2a6398
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-08 20:37:03 -0600

    dispatching section change

--- app/web/containers/StoreGeneralSection.jsx
+++ app/web/containers/StoreGeneralSection.jsx
@@ -73,7 +73,7 @@ const mapDispatchToProps = dispatch => ({
     select() {}
   },
   ordersActions: {
-    select() {}
+    select() { dispatch(changeSection('orderDetail')) }
   },
   productsActions: {
     add() { 
