28921e7ed5a04389bc36338beb4fce2381e573e4
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-08 20:37:18 -0600

    onsave func

--- app/web/containers/OrderDetailSection.jsx
+++ app/web/containers/OrderDetailSection.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import TileHeader from '../components/TileHeader'
 import OrderDetail from '../components/OrderDetail'
 
 const OrderDetailSectionComponent = ({ selectedOrder, onSave }) => (
   <div>
-
+    <OrderDetail order={selectedOrder} onSave={onSave} />
   </div>
 )
 
@@ -19,9 +20,14 @@ const mapStateToProps = state => ({
   selectedOrder: state.orders.selected
 })
 
-const mapDispatchToProps = dispatch ({
+const mapDispatchToProps = dispatch => ({
   onSave() { }
 })
 
-const OrderDetailSection
+const OrderDetailSection = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrderDetailSectionComponent)
+
+export default OrderDetailSection
 
