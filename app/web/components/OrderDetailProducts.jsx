28380baf731996b7c635ed663f84d0d0d8e29570
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-13 11:37:35 -0600

    adding structure for productDetails

--- app/web/components/OrderDetailProducts.jsx
+++ app/web/components/OrderDetailProducts.jsx
@@ -10,29 +10,53 @@ import {
   TableRowColumn
 } from 'material-ui/Table'
 
-const OrderDetailProducts = ({ products }) => (
-  <Table>
-    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-      <TableRow>
-        <TableHeaderColumn>Product Name</TableHeaderColumn>
-        <TableHeaderColumn>Quantity</TableHeaderColumn>
-        <TableHeaderColumn>Price</TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
-    <TableBody
-      displayRowCheckbox={false}
-    >
-      {products.map((prod, index) =>  
-        <TableRow>
-          <TableRowColumn> {prod.name} </TableRowColumn>
-          <TableRowColumn> {prod.quantity} </TableRowColumn>
-          <TableRowColumn> US$ {prod.price} </TableRowColumn>
-          
-        </TableRow>
-      )}
-    </TableBody>
-  </Table>
-)
+import gridStyles from '../styles/grid'
+
+class OrderDetailProducts extends React.Component { 
+  constructor(props) {
+    super(props)
+    this.state = { selected: undefined }
+
+    this.handleSelection = this.handleSelection.bind(this)
+  }
+
+  handleSelection(row) {
+    console.log(row)
+  }
+  
+  render() {
+    return (
+      <div className={gridStyles['flex-col__container']}>
+        <div className={gridStyles['flex-col__half']}> 
+          <Table
+            onRowSelection={this.handleSelection}
+          >
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn>Product Name</TableHeaderColumn>
+                <TableHeaderColumn>Quantity</TableHeaderColumn>
+                <TableHeaderColumn>Price</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody
+              displayRowCheckbox={false}
+            >
+              {this.props.products.map((prod, index) =>  
+                <TableRow>
+                  <TableRowColumn> {prod.name} </TableRowColumn>
+                  <TableRowColumn> {prod.quantity} </TableRowColumn>
+                  <TableRowColumn> US$ {prod.price} </TableRowColumn>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className={gridStyles['flex-col__half']}> 
+        </div>
+      </div>
+    )
+  }
+}
 
 OrderDetailProducts.propTypes = {
   products: PropTypes.array.isRequired
