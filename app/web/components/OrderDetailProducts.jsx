e1bec0abc84efb406d80ea13fc14171058e8678e
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-13 10:41:32 -0600

    OrderDetailProducts component basic structure

---
+++ app/web/components/OrderDetailProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const OrderDetailProducts = ({}) => (
+  <div> Hello OrderDetailProducts </div>
+)
+
+OrderDetailProducts.propTypes = {
+  products: PropTypes.array.isRequired
+}
+
+export default OrderDetailProducts
+
