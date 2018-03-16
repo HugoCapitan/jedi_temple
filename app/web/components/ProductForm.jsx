0dac3c599a8cf1b7e872fc7204c19f9579623a2c
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-16 14:04:44 -0600

    button to save product

--- app/web/components/ProductForm.jsx
+++ app/web/components/ProductForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import FlatButton from 'material-ui/FlatButton'
 import TextField from 'material-ui/TextField'
 
 import ProductCustomsForm from './ProductCustomsForm'
 
+import formStyles from '../styles/form'
 import gridStyles from '../styles/grid'
 
 class ProductForm extends React.Component {
@@ -31,46 +33,51 @@ class ProductForm extends React.Component {
   render() {
     return (
       <div className={gridStyles['container']}>
-        <div className={gridStyles['half-column']}>
-          <TextField
-            floatingLabelText="Name"
-            fullWidth={true}
-            name="name"
-            onChange={this.handleChange}
-            value={this.state.name}
-          />
-          <TextField
-            floatingLabelText="Price (US$)"
-            fullWidth={true}
-            name="price"
-            onChange={this.handleChange}
-            type="number"
-            value={this.state.price}
-          />
-          <TextField
-            floatingLabelText="Stock"
-            fullWidth={true}
-            name="stock"
-            onChange={this.handleChange}
-            type="number"
-            value={this.state.stock}
-          />
-          <TextField
-            floatingLabelText="Description"
-            fullWidth={true}
-            multiLine={true}
-            name="description"
-            onChange={this.handleChange}
-            rows={5}
-            value={this.state.description}
-          />
+        <div>
+          <div className={gridStyles['half-column']}>
+            <TextField
+              floatingLabelText="Name"
+              fullWidth={true}
+              name="name"
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+            <TextField
+              floatingLabelText="Price (US$)"
+              fullWidth={true}
+              name="price"
+              onChange={this.handleChange}
+              type="number"
+              value={this.state.price}
+            />
+            <TextField
+              floatingLabelText="Stock"
+              fullWidth={true}
+              name="stock"
+              onChange={this.handleChange}
+              type="number"
+              value={this.state.stock}
+            />
+            <TextField
+              floatingLabelText="Description"
+              fullWidth={true}
+              multiLine={true}
+              name="description"
+              onChange={this.handleChange}
+              rows={5}
+              value={this.state.description}
+            />
+          </div>
+          <div className={gridStyles['half-column']}>
+            <ProductCustomsForm 
+              customs={this.props.customs}
+              prodCustoms={this.state.customs}
+              reportChange={this.handleCustomsChange}
+            />
+          </div>
         </div>
-        <div className={gridStyles['half-column']}>
-          <ProductCustomsForm 
-            customs={this.props.customs}
-            prodCustoms={this.state.customs}
-            reportChange={this.handleCustomsChange}
-          />
+        <div className={formStyles['bottom-actions']}>
+          <FlatButton label="Save" primary={true} />
         </div>
       </div>
     )
