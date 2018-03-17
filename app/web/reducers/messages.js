59b62467d511b39059f3ca71abe71088fab38236
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-16 19:26:04 -0600

    Reducer for messages

---
+++ app/web/reducers/messages.js
@@ -0,0 +1,31 @@
+import { REQUEST_MESSAGES, RECEIVE_MESSAGES, SELECT_MESSAGE } from '../constants'
+
+const initialState = {
+  isFetching: false,
+  items: [],
+  selected: undefined
+}
+
+function orders (state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_MESSAGES:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case RECEIVE_MESSAGES:
+      return {
+        ...state,
+        items: action.items.reduce((acc, message) => ({...acc, [message._id]: message }), {})
+      }
+    case SELECT_MESSAGE:
+      return {
+        ...state,
+        selected: state.items[action.messageID]
+      }
+    default:
+      return state
+  }
+}
+
+export default message
