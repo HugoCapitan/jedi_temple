5ddfb04ace5945a4a92ebe4b65ce08baea4623c0
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-16 18:22:25 -0600

    middleware functions for message model

--- app/api/models/Message.js
+++ app/api/models/Message.js
@@ -26,6 +26,28 @@ const MessageSchema = new Schema({
   updated_at: Date
 })
 
+MessageSchema._middlewareFuncs = {
+  preSave(next) {
+    const currentDate = new Date()
+
+    this.updated_at = currentDate
+    if (!this.created_at) this.created_at = currentDate
+
+    if (this.isNew) this.read = false
+
+    next()
+  },
+  preUpdate(next) {
+    this.updated_at = new Date()
+
+    next()
+  }
+}
+
+MessageSchema.pre('save', MessageSchema._middlewareFuncs.preSave)
+MessageSchema.pre('update', MessageSchema._middlewareFuncs.preUpdate)
+MessageSchema.pre('findOneAndUpdate', MessageSchema._middlewareFuncs.preUpdate)
+
 const Message = mongoose.model('Message', MessageSchema)
 
 module.exports = Message
