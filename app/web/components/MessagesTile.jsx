92a280c5b5e20f52ba40b5e3af7f19e2a5907887
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-16 19:52:15 -0600

    MessagesTile component basic structure

---
+++ app/web/components/MessagesTile.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import Paper from 'material-ui/Paper'
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
+import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar'
+
+class MessagesTile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <paper></paper>
+    )
+  }
+}
+
+MessagesTile.propTypes = {
+}
+
+export default MesssagesTile
+
