c69fe767726ccbc0b50d1e23bd86f61de90b0490
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-18 08:55:34 -0600

    fixing warnings

--- app/web/reducers/ui.js
+++ app/web/reducers/ui.js
@@ -1,10 +1,8 @@
 import { 
   CHANGE_DRAWER_OPEN,
   CHANGE_SECTION,
-  CLOSE_ITEM_DIALOG,
   FAILED_REQUEST,
   FINISH_REQUEST,
-  OPEN_ITEM_DIALOG,
   START_REQUEST,
   TOGGLE_DRAWER
 } from '../constants'
@@ -28,22 +26,10 @@ const ui = (state = initialState, action) => {
       return { ...state, drawerOpen: action.open }
     case CHANGE_SECTION:
       return { ...state, section: action.section }
-    case CLOSE_ITEM_DIALOG:
-      return { ...state, itemDialog: {
-        open: false,
-        itemClass: '',
-        itemID: ''
-      } }
     case FAILED_REQUEST: 
       return { ...state, isRequestOngoing: false, requestError: action.message }
     case FINISH_REQUEST:
       return { ...state, isRequestOngoing: false }
-    case OPEN_ITEM_DIALOG:
-      return { ...state, itemDialog: { 
-        open: true,
-        itemClass: action.itemClass,
-        itemID: action.itemID,
-      } }
     case START_REQUEST: 
       return { ...state, isRequestOngoing: true }
     case TOGGLE_DRAWER: 
