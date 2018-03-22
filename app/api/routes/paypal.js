fba0b189bedc7d6e933b4b82ca58f6710076b90e
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-22 11:38:09 -0600

    Exposing paypal getExperiences route

--- app/api/routes/paypal.js
+++ app/api/routes/paypal.js
@@ -2,4 +2,5 @@ const paypalCtrl = require('../controllers/paypalCtrl')
 
 module.exports = app => {
   app.post('/paypal/payments/', paypalCtrl.createPaymentEndpoint)
+  app.get('/paypal/experiences/', paypalCtrl.getRemoteExperiencesEndpoint)
 }
