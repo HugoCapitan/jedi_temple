e1d086522b8bb87e03149f21c4c269b1d8782197
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2018-03-25 21:08:32 -0600

    showing route

--- app/api/routes/unahil.js
+++ app/api/routes/unahil.js
@@ -3,4 +3,5 @@ const unahilApiCtrl = require('../controllers/unahilApiCtrl')
 module.exports = server => {
   server.post('/unahil/reservation', unahilApiCtrl.makeReservation)
   server.get('/unahil/execute_payment/:token/', unahilApiCtrl.executePayment)
+  server.get('/unahil/calendar', unahilApiCtrl.getCalendar)
 }
